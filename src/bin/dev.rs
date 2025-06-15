8349a9eed5b5ab48c5ac391fb0041ee0de1f6389
autumn <cyancirrus@gmail.com>
2025-06-14 19:45:07 -0500

    close

--- src/bin/dev.rs
+++ src/bin/dev.rs
@@ -70,129 +70,68 @@ fn butterfly(x:Complex, y:Complex) -> (Complex, Complex) {
     (x + y, x - y)
 }
 
-// fn cooley_tukey(x:&mut [Complex], n:usize, s:usize) {
-//     let mut p:Complex;
-//     let mut q:Complex;
-//     println!("This is the value of n: {}", n);
-//     if n > 1 {
-//         cooley_tukey(&mut x[..], n / 2, 2 * s);
-//         // println!("left inner n: {}", n);
-//         // cooley_tukey(&mut x[s..], n / 2, 2 * s);
-//         cooley_tukey(&mut x[..], n / 2, 2 * s);
-//         // println!("right inner n: {}", n);
-//         println!("this is what x looks like {:?}", x);
-//         for k in 0..n/2 {
-//         // for k in 0..1 {
-//             p = x[k];
-//             // q = twiddle(k as f32, n as f32) * x[k + n /2];
-//             q = twiddle(k as f32, n as f32) * x[k + s];
-//             println!("mutating! x[{}] and x[{}]", k, k + s);
-//             x[k] = p + q;
-//             x[k + n/2] = p - q;
-//             // println!("k: {}, k-o: {}, p:{}, q:{}", k, k + n/2, p, q);
-//             println!("p:{}, q:{}", p, q);
-//         }
-
-//     }
-// }
-
-fn cooley_tukey(x:&mut [Complex], n:usize, s:usize) {
-    let mut p:Complex;
-    let mut q:Complex;
-    println!("This is the value of n: {}", n);
-    if n > 1 {
-        cooley_tukey(&mut x[..], n / 2, 2 * s);
-        // println!("left inner n: {}", n);
-        // cooley_tukey(&mut x[s..], n / 2, 2 * s);
-        cooley_tukey(&mut x[..], n / 2, 2 * s);
-        // println!("right inner n: {}", n);
-        println!("this is what x looks like {:?}", x);
-        for k in 0..n/2 {
-        // for k in 0..1 {
-            p = x[k];
-            // q = twiddle(k as f32, n as f32) * x[k + n /2];
-            q = twiddle(k as f32, n as f32) * x[k + s];
-            println!("mutating! x[{}] and x[{}]", k, k + s);
-            x[k] = p + q;
-            x[k + n/2] = p - q;
-            // println!("k: {}, k-o: {}, p:{}, q:{}", k, k + n/2, p, q);
-            println!("p:{}, q:{}", p, q);
-        }
-
-    }
-}
-
-// fn g(mut x: Vec<Complex>, indices:impl Iterator<Item = usize> + Clone, n:usize, s:usize) {
-//     let length = x.len();
-//     let mut p:Complex;
-//     let mut q:Complex;
-//     if x.len() > 1 {
-//         for k in 0..n/2 {
-//             p = x[k];
-//             q = twiddle(k, n) * x[k + n/2];
-//         }
-        
-//         g(x, (0..length).step_by(n/2), n/2, s * 2);
-//         g(x, (1..length+1).step_by(n/2), n/2, s * 2);
-//     }
-// }
-
 fn twiddle(k:usize,n:usize) -> Complex {
     // exp( (-2 * pi * i * k / n)
     // = cos(*) - isin(*)
-    // let phase= 2_f32 * PI * k / n;
     let phase= 2_f32 * PI * k as f32 / n as f32;
     let a = Complex::new(phase.cos(), -phase.sin());
-    println!("Phase pi normalized {}, {:?}", phase/PI, a);
-    a
+    println!("Phase pi normalized {}, {:?}", phase/PI, a); a
+}
+
+
+// fn cooley_tukey(o:usize, n:usize, s:usize, x:&mut [Complex]) {
+//     if n == 1 {
+//         return;
+//     }
+//     let mut p:Complex;
+//     let mut q:Complex;
+//     let half= n>>1;
+//     let doub= s<<1;
+//     cooley_tukey(o, half, doub, x);
+//     cooley_tukey(o + s,  half, doub, x);
+//     println!("this is what x looks like {:?}", x);
+
+//     for k in o..half {
+//         // println!("mutating! x[{}] and x[{}]", ei, oi);
+//         let ei = o + k * 2 * s;
+//         let oi = ei + s;
+//         p = x[ei];
+//         q = twiddle(k, n) * x[oi];
+        
+//         x[ei] = p + q;
+//         x[oi] = p - q;
+//     }
+// }
+
+fn cooley_tukey(start: usize, n: usize, stride: usize, x: &mut [Complex]) {
+    if n == 1 {
+        // Base case: one element is already transformed
+        return;
+    }
+    let mut p:Complex;
+    let mut q:Complex;
+    let half= n>>1;
+    let doub= stride<<1;
+
+    cooley_tukey(start, half, doub, x);
+    cooley_tukey(start + stride,  half, doub, x);
+
+    for k in 0..half {
+        let ei= start + k * doub;
+        let oi = ei + stride;
+
+        let q = twiddle(k, n) * x[oi];
+        let p = x[ei];
+        x[ei] = p + q;
+        x[oi] = p - q;
+    }
 }
 
 
 
-// fn cooley_tukey(x: &mut Vec<Complex>, n:usize, s:usize, odd:bool) {
-//     let length = x.len();
-//     let mut p:Complex;
-//     let mut q:Complex;
-//     let mut odds:Vec<usize>;
-//     let mut evens:Vec<usize>;
-//     println!("----------------------------------");
-//     if n > 1 {
-//         // for k in 0..n/2 {
-//         for k in 0..n/2 {
-//             if odd { 
-//                 println!("N:{}, S:{}", n, s);
-//                 p = x[k];
-//                 q = twiddle(k, s) * x[k + n/2];
-//                 x[k] = p + q;
-//                 // x[s + k] = p - q;
-//                 x[k + length/2] = p - q;
-//                 println!("input! x[{}] and x[{}], k {}", k, k + n/2, k);
-//                 println!("target! x[{}] and x[{}], k {}", k, k + length/2, k);
-//                 println!("x: {:?}", x);
-//                 println!("----------------------------------");
-//             } else { 
-//                 println!("N:{}, S:{}", n, s);
-//                 p = x[k+ 1];
-//                 q = twiddle(k, s) * x[k + 1+ n/2];
-//                 x[k] = p + q;
-//                 // x[s + k] = p - q;
-//                 x[k + length/2] = p - q;
-//                 println!("input! x[{}] and x[{}], k {}", k + 1, k + 1 + n/2, k);
-//                 println!("target! x[{}] and x[{}], k {}", k + 1, k + 1 + length/2, k);
-//                 println!("x: {:?}", x);
-//                 println!("----------------------------------");
-//         }
-//         }
-//         cooley_tukey(x,  n/2, s * 2, true);
-//         cooley_tukey(x,  n/2, s * 2, false);
-//     }
-// }
-
 fn fft_algorithm(mut x:Vec<Complex>) -> Vec<Complex> {
     let n = x.len();
-    let indices = (0..n).collect::<Vec<usize>>();
-    // cooley_tukey(&mut x, indices, n, 1);
-    cooley_tukey(&mut x, n, 1, true);
+    cooley_tukey(0, n, 1, &mut x);
     x
 }
 
