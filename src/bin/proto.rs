63db8dd645da5731dd869c245d1253ea2341e91d
autumn <cyancirrus@gmail.com>
2025-06-16 20:45:18 -0500

    nd transposes are so much harder than i thought

--- src/bin/proto.rs
+++ src/bin/proto.rs
@@ -23,82 +23,64 @@ fn offset(dims:&[usize]) -> Vec<usize> {
 
 fn transpose_clean<T>(data:&mut[T]) {
     let dims = vec![2,2,2];
+    //  coming in to the function
+    let n = dims.iter().product();
     let card = dims.len();
-    let offset = [4,2,1];
-    let mut cycle = vec![0;card];
-    let mut mem = vec![false; dims.iter().product()];
-    let mut state = vec![0;card];
+    let offset = vec![4,2,1];
     let mut cursor = 0;
+    let mut cycle = vec![0; card];
+    let mut mem = vec![false; n];
+    let mut state = vec![0;card];
     let mut s;
 
-    while cursor < card {
-        if state[cursor] != 0 {
-            for p in 0..card {
-                state[p] += offset[(cursor + p) % card];
-            }
-        }
+    for _ in 0..n {
+        println!("State {:?}", state);
+        println!("Cycle {:?}", cycle);
         if !mem[cycle[0]] {
             mem[cycle[0]] = true;
             for p in 1..card {
+                mem[cycle[p]] = true;
                 data.swap(cycle[p-1], cycle[p]);
             }
         }
+        // we're not to the capacity for the dimension increment the index
+        // this is like k+=1 in 3dimensions
+        if state[cursor] + 1 < dims[cursor] {
+            state[cursor] += 1;
+            // incrementing like in the 3d case
+            for g in 0..card {
+                cycle[g] += offset[(cursor + g) % card];
+            }
+            continue;
+        }
+        // oh no! we're out of capacity we need to handle
         loop {
+            // increment the position to see if we can add another dimension if it has capacity
+            cursor += 1;
             if cursor == card {
-                break;
+                return;
             }
             if state[cursor] + 1 < dims[cursor] {
+                // found an open slot
                 state[cursor] += 1;
-                for idx in 0..cursor {
-                    state[idx] = 0;
-                    for l in 0..card {
-                        s = dims[cursor] - 1;
-                        cycle[idx] -= s * offset[(l +idx) % card];
-                    }
+                // decrementing like in the 3d case
+                for g in 0..card {
+                    cycle[g] += offset[(cursor + g) % card];
                 }
-                cursor=0;
                 break;
-            } else {
-                cursor += 1;
+            }
+        };
+        // clearing out the lower bits
+        for idx in 0..cursor {
+            state[idx]=0;
+            // decrementing for all of those bits which were cleared
+            for g in 0..card {
+                s = dims[idx]-1;
+                cycle[g] -= s * offset[(idx + g) % card];
             }
         }
+        cursor=0;
     }
-    // for i in 0..dims[0] {
-    //     if i > 0 {
-    //         incrementer[0] += offset[0];
-    //         incrementer[1] += offset[1];
-    //         incrementer[2] += offset[2];
-    //     }
-    //     for j in 0..dims[1] {
-    //         if j > 0 {
-    //             incrementer[0] += offset[1];
-    //             incrementer[1] += offset[2];
-    //             incrementer[2] += offset[0];
-    //         }
-    //         for k in 0..dims[2] {
-    //             if k > 0 {
-    //                 incrementer[0] += offset[2];
-    //                 incrementer[1] += offset[0];
-    //                 incrementer[2] += offset[1];
-    //             }
-    //             if !mem[incrementer[0]] {
-    //                 mem[incrementer[0]]=true;
-    //                 for i in 1..card {
-    //                     data.swap(incrementer[i-1], incrementer[i]);
-    //                     mem[incrementer[i]]=true;
-    //                 }
-    //             }
-    //         }
-    //         s = dims[2] - 1;
-    //         incrementer[0] -= s * offset[2];
-    //         incrementer[1] -= s * offset[0];
-    //         incrementer[2] -= s * offset[1];
-    //     }
-    //     s = dims[1] - 1;
-    //     incrementer[0] -= s * offset[1];
-    //     incrementer[1] -= s * offset[2];
-    //     incrementer[2] -= s * offset[0];
-    // }
 }
 
 fn transpose_ideation<T>(data:&mut[T]) {
@@ -118,28 +100,86 @@ fn transpose_ideation<T>(data:&mut[T]) {
                         mem[cycle[i]]=true;
                     }
                 }
+                // variable 2 + offset 2
                 cycle[0] += offset[2];
                 cycle[1] += offset[0];
                 cycle[2] += offset[1];
             }
+            // variable 2 for - offset 2
             s = dims[2];
             cycle[0] -= s * offset[2];
             cycle[1] -= s * offset[0];
             cycle[2] -= s * offset[1];
+            // variable 1 for + offset 1
             cycle[0] += offset[1];
             cycle[1] += offset[2];
             cycle[2] += offset[0];
         }
+        // variable 1 for - offset 1
         s = dims[1];
         cycle[0] -= s * offset[1];
         cycle[1] -= s * offset[2];
         cycle[2] -= s * offset[0];
+        // variable 0 for + offset 0
         cycle[0] += offset[0];
         cycle[1] += offset[1];
         cycle[2] += offset[2];
     }
 }
 
+
+
+fn pretty_print(data:&[usize]) {
+    println!("----------");
+    for i in (0..8).step_by(2) {
+        if i == 4 {
+            println!(" - - - - - -");
+        }
+        println!("[ {:03b}, {:03b} ]", data[i], data[i+1]);
+    }
+    println!("----------");
+}
+
+fn debug_transpose(data:&mut [usize]) {
+    transpose_clean(data);
+    pretty_print(data);
+}
+
+fn debug_data() -> Vec<usize> {
+    // dimensions indexed by (x,y,z)
+    vec![
+        0, 2,
+        4, 6,
+        //---
+        1, 3,
+        5, 7 
+    ]
+}
+
+fn main() {
+    let data = debug_data();
+    let mut test = debug_data();
+    pretty_print(&test);
+    debug_transpose(&mut test);
+    debug_transpose(&mut test);
+    debug_transpose(&mut test);
+    assert_eq!(data, test);
+
+    // thingy();
+
+    // // println!("dims {:?}", get_inner(&[5,4,3,2,1]));
+    // assert_eq!(vec![1, 2, 6, 24], get_inner(&[5,4,3,2,1]));
+    // let mut data = [1,2,3,4];
+    // transpose_test(&mut data);
+    // assert_eq!(data, [2,3,4,1]);
+    // transpose_test(&mut data);
+    // assert_eq!(data, [3,4,1,2]);
+    // transpose_test(&mut data);
+    // assert_eq!(data, [4,1,2,3]);
+    // transpose_test(&mut data);
+    // assert_eq!(data, [1,2,3,4]);
+    // println!("new prototype function?");
+}
 
 // fn transpose_ideation<T>(data:&mut[T]) {
 //     let dims = vec![2,2,2];
@@ -232,55 +272,3 @@ fn transpose_ideation<T>(data:&mut[T]) {
 //         }
 //     }
 // }
-
-fn pretty_print(data:&[usize]) {
-    println!("----------");
-    for i in (0..8).step_by(2) {
-        if i == 4 {
-            println!(" - - - - - -");
-        }
-        println!("[ {:03b}, {:03b} ]", data[i], data[i+1]);
-    }
-    println!("----------");
-}
-
-fn debug_transpose(data:&mut [usize]) {
-    transpose_ideation(data);
-    pretty_print(data);
-}
-
-fn debug_data() -> Vec<usize> {
-    // dimensions indexed by (x,y,z)
-    vec![
-        0, 2,
-        4, 6,
-        //---
-        1, 3,
-        5, 7 
-    ]
-}
-
-fn main() {
-    let data = debug_data();
-    let mut test = debug_data();
-    pretty_print(&test);
-    debug_transpose(&mut test);
-    debug_transpose(&mut test);
-    debug_transpose(&mut test);
-    assert_eq!(data, test);
-
-    // thingy();
-
-    // // println!("dims {:?}", get_inner(&[5,4,3,2,1]));
-    // assert_eq!(vec![1, 2, 6, 24], get_inner(&[5,4,3,2,1]));
-    // let mut data = [1,2,3,4];
-    // transpose_test(&mut data);
-    // assert_eq!(data, [2,3,4,1]);
-    // transpose_test(&mut data);
-    // assert_eq!(data, [3,4,1,2]);
-    // transpose_test(&mut data);
-    // assert_eq!(data, [4,1,2,3]);
-    // transpose_test(&mut data);
-    // assert_eq!(data, [1,2,3,4]);
-    // println!("new prototype function?");
-}
