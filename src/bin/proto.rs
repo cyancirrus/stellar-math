d2d0bd0cac6948ca23a79e3bed05c8d9404aea18
autumn <cyancirrus@gmail.com>
2025-09-18 13:12:01 -0500

    updating versions for rand and what not

--- src/bin/proto.rs
+++ src/bin/proto.rs
@@ -6,78 +6,20 @@ use stellar::structure::ndarray::NdArray;
 use stellar::algebra::ndmethods::create_identity_matrix;
 use stellar::decomposition::householder::householder_factor;
 
+use rand::Rng;
+use rand_distr::StandardNormal;
+// use rand::prelude::*;
+
+
 // #[cfg(target_arch = "x86_64")]
 
-
-const TOLERANCE_CONDITION: f32 = 1e-6;
-
-struct Cholesky {
-    l: NdArray,
-}
-
-struct LU {
-    lower: NdArray,
-    upper: NdArray,
-}
-
-fn lower_upper(mut upper:NdArray) -> LU {
-    // A[j, *] = c *A[i, *]
-    // => c = A[i,j] / A[j,j]
-    let rows = upper.dims[0];
-    let cols = upper.dims[1];
-    debug_assert_eq!(rows, cols);
-    let mut lower = create_identity_matrix(rows);
-
-    for i in 0..cols {
-    for j in i..rows {
-            if upper.data[j*cols + i].abs() < TOLERANCE_CONDITION { continue; }
-            let c = upper.data[j* cols + i] / upper.data[ i * cols + i];
-            lower.data[j * cols + i] = c ;
-            for k in i..cols {
-                upper.data[j * cols + k] -= c * upper.data[i*cols + k];
-            }
-        }
-    }
-    LU {
-        lower,
-        upper
-    }
-}
-
-fn cholesky(a:NdArray) -> Cholesky {
-    // update lij
-    // aii = lik * lik + lii^2
-    // => lii = (aii - sum 0..i lik)^0.5
-    // aij = Sum 0..j lik * ljk + lij * ljj
-    // => ljj = 1/ ljj * (aij - Sum 0..j lik * ljk )
-    // a32 = l31 * l21 + l32 * l22
-    let rows = a.dims[0];
-    let cols = a.dims[1];
-    debug_assert_eq!(rows, cols);
-    let mut l = vec![0_f32; rows * cols];
-
-    for i in 0..rows {
-        for j in 0..=i {
-            let mut sum = 0_f32;
-            for k in 0..j {
-                sum += l[i*cols + k] * l[j*cols + k];
-            }
-            if i==j { l[i * cols + i] = (a.data[i*cols + i] - sum).sqrt(); }
-            else { l[i * cols + j] = (a.data[i * cols + j] - sum) / l[j * cols + j]};
-
-        }
-    }
-    Cholesky {
-        l: NdArray {
-            dims: a.dims.clone(),
-            data:l,
-        }
-    }
-
-}
 
 
 
 fn main() {
+    let mut rng = rand::rng();
+    let normal = StandardNormal;
+    let x:f32 = rng.sample(StandardNormal); 
+    println!("result {x:?}");
 }
 
