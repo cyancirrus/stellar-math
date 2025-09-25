8c2df534f3fecf8c956b785754afdc29d29571c1
autumn <cyancirrus@gmail.com>
2025-09-25 11:37:54 -0500

    schur successfully reconstructs

--- tests/schur_decomposition.rs
+++ tests/schur_decomposition.rs
@@ -2,10 +2,11 @@
 
 mod schur_decomposition {
     use stellar::decomposition::schur::real_schur;
+    use stellar::algebra::ndmethods::tensor_mult;
     use stellar::structure::ndarray::NdArray;
     const TOLERANCE: f32 = 1e-3;
 
-    fn approx_eq(a: &[f32], b: &[f32]) -> bool {
+    fn approx_vector_eq(a: &[f32], b: &[f32]) -> bool {
         a.len() == b.len()
             && a.iter()
                 .zip(b.iter())
@@ -32,18 +33,21 @@ mod schur_decomposition {
 
     #[test]
     fn test_reconstruction() {
-        //TODO: Implement a right multiply
-        // let dims = vec![2, 2];
-        // let data = vec![
-        //     -1.0, 0.0,
-        //      5.0, 2.0,
-        // ];
-        // let x = NdArray::new(dims.clone(), data);
-        // let schur = real_schur(x);
-        // let expected_eigens= vec![ 2.000, -1.000];
-        // for i in 0..dims[0] {
-        //     assert!(approx_scalar_eq(expected_eigens[i], schur.kernel.data[i * (dims[0] + 1)], 1e-3));
+        let dims = vec![2, 2];
+        let data = vec![
+            -1.0, 0.0,
+             5.0, 2.0,
+        ];
+        let x = NdArray::new(dims.clone(), data.clone());
+        let schur = real_schur(x);
+        let q_star = schur.rotation.transpose();
+        let q = &schur.rotation;
 
-        // }
+        let result = tensor_mult(4, &q_star, &schur.kernel);
+        let result = tensor_mult(4, &result, q);
+        assert!(approx_vector_eq(
+            &result.data,
+            &data,
+        ));
     }
 }
