855b8b23daa63808df5cdfdb16e2ca93402d30a6
autumn <cyancirrus@gmail.com>
2025-09-25 14:55:26 -0500

    dont need obsolesced

--- src/decomposition/householder.rs
+++ src/decomposition/householder.rs
@@ -36,29 +36,3 @@ pub fn householder_params(mut u: Vec<f32>) -> HouseholderReflection {
     magnitude_squared += 2_f32 * sign * tmp * magnitude_squared.sqrt() + magnitude_squared;
     HouseholderReflection::new(2_f32 / magnitude_squared, u)
 }
-//pub fn householder_params(x: &[f32]) -> HouseholderReflection {
-//    //TODO: change the above to take ownership of the vector
-//    // already cloning it and QR requires column and golub kahn requires rows
-
-//    // pub fn householder_params(mut x: Vec<f32>) -> HouseholderReflection {
-//    let length = x.len();
-//    let mut max_element = f32::NEG_INFINITY;
-//    let mut magnitude_squared = 0_f32;
-//    for i in 0..length {
-//        max_element = max_element.max(x[i]);
-//    }
-//    if max_element.abs() < EPSILON {
-//        return HouseholderReflection::new(0_f32, vec![0_f32]);
-//    }
-//    let mut u = vec![0_f32; length];
-//    for i in 0..length {
-//        let result = x[i] / max_element;
-//        u[i] = result;
-//        magnitude_squared += result.powi(2);
-//    }
-//    let sign = u[0].signum();
-//    let tmp = u[0];
-//    u[0] += sign * magnitude_squared.sqrt();
-//    magnitude_squared += 2_f32 * sign * tmp * magnitude_squared.sqrt() + magnitude_squared;
-//    HouseholderReflection::new(2_f32 / magnitude_squared, u)
-//}
