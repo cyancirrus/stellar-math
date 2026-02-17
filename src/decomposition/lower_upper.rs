f7393cd46475a6315986397c4c954eb22871dab6
autumn <cyancirrus@gmail.com>
2026-02-17 10:10:08 -0600

    done

--- src/decomposition/lower_upper.rs
+++ src/decomposition/lower_upper.rs
@@ -1,10 +1,10 @@
 use crate::algebra::ndmethods::tensor_mult;
-use crate::decomposition::lu::LuDecomposition;
 use crate::random::generation::generate_random_matrix;
 use crate::structure::ndarray::NdArray;
 
 pub struct LuPivotDecomp {
     n: usize,
+    swaps: usize,
     pivots: Vec<usize>,
     matrix: NdArray,
 }
@@ -15,21 +15,25 @@ impl LuPivotDecomp {
         // => c = A[i,j] / A[j,j]
         debug_assert_eq!(matrix.dims[0], matrix.dims[1]);
         let n = matrix.dims[0];
-        let mut pivots = vec![usize::MAX; n];
+        let mut pivots:Vec<usize> = (0..n).collect();
+        let mut swaps = 0;
 
         // for lij, we need knowledge of ujj due to formula
         // this means that we need u[0..i.min(j)] upper triangular calculated
         for i in 0..n {
-            let mut val = 0.0;
-            for k in i..n {
+            let mut val = matrix.data[ i * n + i].abs();
+            for k in i+1..n {
                 let mag = matrix.data[k * n + i].abs();
-                if mag >= val {
+                if mag > val {
                     pivots[i] = k;
                     val = mag;
                 }
             }
-            for k in 0..n {
-                matrix.data.swap(i * n + k, pivots[i] * n + k);
+            if pivots[i] != i {
+                swaps += 1;
+                for k in 0..n {
+                    matrix.data.swap(i * n + k, pivots[i] * n + k);
+                }
             }
             for j in 0..n {
                 for k in 0..i.min(j) {
@@ -40,13 +44,14 @@ impl LuPivotDecomp {
                 }
             }
         }
-        Self { n, pivots, matrix }
+        Self { n, swaps, pivots, matrix }
     }
-    pub fn new_row(mut matrix: NdArray) -> Self {
+    pub fn new_dl(mut matrix: NdArray) -> Self {
         // Doolittle
         debug_assert_eq!(matrix.dims[0], matrix.dims[1]);
         let n = matrix.dims[0];
         let mut pivots: Vec<usize> = (0..n).collect();
+        let mut swaps = 0;
 
         let mut val;
         for k in 0..n {
@@ -59,6 +64,7 @@ impl LuPivotDecomp {
                 }
             }
             if pivots[k] != k {
+                swaps += 1;
                 for j in 0..n {
                     matrix.data.swap(k * n + j, pivots[k] * n + j);
                 }
@@ -72,20 +78,7 @@ impl LuPivotDecomp {
                 }
             }
         }
-        Self { n, pivots, matrix }
-    }
-    pub fn left_apply_u(&self, mut x: NdArray) -> NdArray {
-        // UA => Output
-        for i in 0..self.n {
-            for j in 0..self.n {
-                x.data[i * self.n + j] *= self.matrix.data[i * self.n + i];
-                for k in i + 1..self.n {
-                    x.data[i * self.n + j] +=
-                        self.matrix.data[i * self.n + k] * x.data[k * self.n + j]
-                }
-            }
-        }
-        x
+        Self { n, swaps, pivots, matrix }
     }
     pub fn reconstruct(&self) -> NdArray {
         let mut data = vec![0_f32; self.n * self.n];
@@ -113,21 +106,197 @@ impl LuPivotDecomp {
         }
         NdArray { dims, data }
     }
+    pub fn determinant(&self) -> f32 {
+        let mut det = 1f32;
+        for k in 0..self.n {
+            det *= self.matrix.data[ k * self.n + k];
+        }
+        if self.swaps & 1 == 1 {
+            det = -det
+        }
+        det
+    }
+}
+impl LuPivotDecomp {
+    pub fn find_determinant(&self) -> f32 {
+        // |A B| = |A| |B|
+        // = diag |A| diag |B|
+        // = diag |B|
+        let n = self.matrix.dims[0];
+        let mut det = 1_f32;
+        for i in 0..n {
+            det *= self.matrix.data[i * n + i];
+        }
+        det
+    }
+    // for matrices
+    pub fn left_apply_l(&self, target: &mut NdArray) {
+        // LA = Output
+        debug_assert_eq!(target.dims[0], self.matrix.dims[1]);
+        let (rows, cols) = (self.matrix.dims[0], self.matrix.dims[1]);
+        let (trows, tcols) = (target.dims[0], target.dims[1]);
+        for i in (1..rows).rev() {
+            for j in 0..tcols {
+                // lii == 1
+                for k in 0..i {
+                    target.data[i * tcols + j] +=
+                        self.matrix.data[i * cols + k] * target.data[k * tcols + j];
+                }
+            }
+        }
+    }
+    pub fn left_apply_u(&self, target: &mut NdArray) {
+        // UA = Output
+        debug_assert_eq!(target.dims[0], self.matrix.dims[1]);
+        let (rows, cols) = (self.matrix.dims[0], self.matrix.dims[1]);
+        let (trows, tcols) = (target.dims[0], target.dims[1]);
+        for i in 0..rows {
+            for j in 0..tcols {
+                target.data[i * tcols + j] *= self.matrix.data[i * cols + i];
+                for k in i + 1..cols {
+                    target.data[i * tcols + j] +=
+                        self.matrix.data[i * cols + k] * target.data[k * tcols + j];
+                }
+            }
+        }
+    }
+    pub fn right_apply_l(&self, target: &mut NdArray) {
+        // AL = Output
+        debug_assert_eq!(target.dims[1], self.matrix.dims[0]);
+        let (rows, cols) = (self.matrix.dims[0], self.matrix.dims[1]);
+        let (trows, tcols) = (target.dims[0], target.dims[1]);
+        for i in 0..trows {
+            for j in 0..rows {
+                for k in j + 1..rows {
+                    target.data[i * tcols + j] +=
+                        target.data[i * tcols + k] * self.matrix.data[k * cols + j];
+                }
+            }
+        }
+    }
+    pub fn right_apply_u(&self, target: &mut NdArray) {
+        // AU = Output
+        debug_assert_eq!(target.dims[1], self.matrix.dims[0]);
+        let (rows, cols) = (self.matrix.dims[0], self.matrix.dims[1]);
+        let (trows, tcols) = (target.dims[0], target.dims[1]);
+        for i in 0..trows {
+            for j in (0..rows).rev() {
+                target.data[i * tcols + j] *= self.matrix.data[j * cols + j];
+                for k in 0..j {
+                    target.data[i * tcols + j] +=
+                        target.data[i * tcols + k] * self.matrix.data[k * cols + j];
+                }
+            }
+        }
+    }
 }
 
-// a [4] [ 2]
-// 1 -> 2, 2 -> 3, 3 -> 1
+impl LuPivotDecomp {
+    pub fn left_apply_l_vec(&self, target: &mut [f32]) {
+        // Lx
+        debug_assert_eq!(self.matrix.dims[1], target.len());
+        let (rows, cols) = (self.matrix.dims[0], self.matrix.dims[1]);
+        for i in (0..rows).rev() {
+            for k in 0..i {
+                target[i] += self.matrix.data[i * cols + k] * target[k];
+            }
+        }
+    }
+    pub fn left_apply_u_vec(&self, target: &mut [f32]) {
+        // Ux
+        debug_assert_eq!(self.matrix.dims[1], target.len());
+        let (rows, cols) = (self.matrix.dims[0], self.matrix.dims[1]);
+        for i in 0..rows {
+            target[i] *= self.matrix.data[i * cols + i];
+            for k in i + 1..cols {
+                target[i] += self.matrix.data[i * cols + k] * target[k];
+            }
+        }
+    }
+    pub fn right_apply_l_vec(&self, target: &mut [f32]) {
+        //x'L
+        debug_assert_eq!(self.matrix.dims[1], target.len());
+        let (rows, cols) = (self.matrix.dims[0], self.matrix.dims[1]);
+        for j in 0..cols {
+            for k in j + 1..rows {
+                target[j] += target[k] * self.matrix.data[k * cols + j]
+            }
+        }
+    }
+    pub fn right_apply_u_vec(&self, target: &mut [f32]) {
+        //x'U
+        debug_assert_eq!(self.matrix.dims[1], target.len());
+        let (rows, cols) = (self.matrix.dims[0], self.matrix.dims[1]);
+        for j in (0..cols).rev() {
+            target[j] *= self.matrix.data[j * cols + j];
+            for k in 0..j {
+                target[j] += target[k] * self.matrix.data[k * cols + j]
+            }
+        }
+    }
+}
 
-// fn test_reconstruct() {
-//     let n = 4;
-//     let x = generate_random_matrix(n, n);
-//     println!("x {x:?}");
-//     let lu = LuPivotDecomp::new(x.clone());
-//     // let lu = LuDecomposition::new(x.clone());
-//     let out = lu.reconstruct();
-//     assert_eq!(x, out);
-// }
+impl LuPivotDecomp {
+    // Ax = y;
+    // LuDecomposition*x = y;
+    // Lz = y -> z;
+    // Ux = z -> x;
+    // => x
 
-// fn main() {
-//     test_reconstruct();
-// }
+    pub fn solve_inplace(&self, y: &mut NdArray) {
+        self.forward_solve_inplace(y);
+        self.backward_solve_inplace(y);
+    }
+    pub fn solve_inplace_vec(&self, y: &mut [f32]) {
+        self.forward_solve_inplace_vec(y);
+        self.backward_solve_inplace_vec(y);
+    }
+    pub fn forward_solve_inplace(&self, y: &mut NdArray) {
+        // transforms y -> z
+        debug_assert_eq!(self.matrix.dims[1], y.dims[0]);
+        let (rows, cols) = (self.matrix.dims[0], self.matrix.dims[1]);
+        let (trows, tcols) = (y.dims[0], y.dims[1]);
+        for j in 0..tcols {
+            for i in 0..rows {
+                for k in 0..i {
+                    y.data[i * tcols + j] -= self.matrix.data[i * cols + k] * y.data[k * tcols + j];
+                }
+            }
+        }
+    }
+    pub fn backward_solve_inplace(&self, z: &mut NdArray) {
+        // transforms y -> z
+        debug_assert_eq!(self.matrix.dims[1], z.dims[0]);
+        let (rows, cols) = (self.matrix.dims[0], self.matrix.dims[1]);
+        let (trows, tcols) = (z.dims[0], z.dims[1]);
+        for i in (0..rows).rev() {
+            for j in 0..tcols {
+                for k in i + 1..cols {
+                    z.data[i * tcols + j] -= self.matrix.data[i * cols + k] * z.data[k * tcols + j];
+                }
+                z.data[i * cols + j] /= self.matrix.data[i * cols + i];
+            }
+        }
+    }
+    pub fn forward_solve_inplace_vec(&self, y: &mut [f32]) {
+        // transforms y -> z
+        debug_assert_eq!(self.matrix.dims[1], y.len());
+        let cols = self.matrix.dims[1];
+        for i in 0..cols {
+            for k in 0..i {
+                y[i] -= self.matrix.data[i * cols + k] * y[k]
+            }
+        }
+    }
+    pub fn backward_solve_inplace_vec(&self, z: &mut [f32]) {
+        // transforms z -> x
+        debug_assert_eq!(self.matrix.dims[1], z.len());
+        let cols = self.matrix.dims[1];
+        for i in (0..cols).rev() {
+            for k in i + 1..cols {
+                z[i] -= self.matrix.data[i * cols + k] * z[k]
+            }
+            z[i] /= self.matrix.data[i * cols + i]
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,129 +1,15 @@
 #![allow(dead_code, unused_imports)]
 
 use stellar::algebra::ndmethods::tensor_mult;
-use stellar::decomposition::lu::LuDecomposition;
+use stellar::decomposition::lower_upper::LuPivotDecomp;
 use stellar::random::generation::generate_random_matrix;
 use stellar::structure::ndarray::NdArray;
-
-pub struct LuPivotDecomp {
-    n: usize,
-    pivots: Vec<usize>,
-    matrix: NdArray,
-}
-impl LuPivotDecomp {
-    pub fn new(mut matrix: NdArray) -> Self {
-        // Croute
-        // A[j, *] = c *A[i, *]
-        // => c = A[i,j] / A[j,j]
-        debug_assert_eq!(matrix.dims[0], matrix.dims[1]);
-        let n = matrix.dims[0];
-        let mut pivots = vec![usize::MAX; n];
-
-        // for lij, we need knowledge of ujj due to formula
-        // this means that we need u[0..i.min(j)] upper triangular calculated
-        for i in 0..n {
-            let mut val = 0.0;
-            for k in i..n {
-                let mag = matrix.data[k * n + i].abs();
-                if mag >= val {
-                    pivots[i] = k;
-                    val = mag;
-                }
-            }
-            for k in 0..n {
-                matrix.data.swap(i * n + k, pivots[i] * n + k);
-            }
-            for j in 0..n {
-                for k in 0..i.min(j) {
-                    matrix.data[i * n + j] -= matrix.data[i * n + k] * matrix.data[k * n + j]
-                }
-                if i > j {
-                    matrix.data[i * n + j] /= matrix.data[j * n + j];
-                }
-            }
-        }
-        Self { n, pivots, matrix }
-    }
-    pub fn new_row(mut matrix: NdArray) -> Self {
-        // Doolittle
-        debug_assert_eq!(matrix.dims[0], matrix.dims[1]);
-        let n = matrix.dims[0];
-        let mut pivots: Vec<usize> = (0..n).collect();
-
-        let mut val;
-        for k in 0..n {
-            val = 0.0;
-            for i in k + 1..n {
-                let mag = matrix.data[i * n + k].abs();
-                if mag >= val {
-                    pivots[k] = i;
-                    val = mag;
-                }
-            }
-            if pivots[k] != k {
-                for j in 0..n {
-                    matrix.data.swap(k * n + j, pivots[k] * n + j);
-                }
-            }
-            for i in k + 1..n {
-                matrix.data[i * n + k] /= matrix.data[k * n + k];
-            }
-            for i in k + 1..n {
-                for j in k + 1..n {
-                    matrix.data[i * n + j] -= matrix.data[i * n + k] * matrix.data[k * n + j]
-                }
-            }
-        }
-        Self { n, pivots, matrix }
-    }
-    pub fn left_apply_u(&self, mut x: NdArray) -> NdArray {
-        // UA => Output
-        for i in 0..self.n {
-            for j in 0..self.n {
-                x.data[i * self.n + j] *= self.matrix.data[i * self.n + i];
-                for k in i + 1..self.n {
-                    x.data[i * self.n + j] +=
-                        self.matrix.data[i * self.n + k] * x.data[k * self.n + j]
-                }
-            }
-        }
-        x
-    }
-    pub fn reconstruct(&self) -> NdArray {
-        let mut data = vec![0_f32; self.n * self.n];
-        let dims = vec![self.n; 2];
-        for i in 0..self.n {
-            for j in 0..self.n {
-                // if i > j then we hit an add at a[i][k] b/c diagonal is identity;
-                for k in 0..=i.min(j) {
-                    if k == i {
-                        data[i * self.n + j] += self.matrix.data[i * self.n + j];
-                    } else {
-                        data[i * self.n + j] +=
-                            self.matrix.data[i * self.n + k] * self.matrix.data[k * self.n + j];
-                    }
-                }
-            }
-        }
-        for (n, &k) in self.pivots.iter().enumerate().rev() {
-            if n == k {
-                continue;
-            }
-            for j in 0..self.n {
-                data.swap(n * self.n + j, k * self.n + j);
-            }
-        }
-        NdArray { dims, data }
-    }
-}
-
-// a [4] [ 2]
-// 1 -> 2, 2 -> 3, 3 -> 1
 
 fn test_reconstruct() {
     let n = 4;
     let x = generate_random_matrix(n, n);
     let lu = LuPivotDecomp::new(x.clone());
+    // let lu = LuPivotDecomp::new_dl(x.clone());
     // let lu = LuDecomposition::new(x.clone());
     let out = lu.reconstruct();
     assert_eq!(x, out);
