a8e4d5442210312e3f9974572c2ef3b09bc7b707
autumn <cyancirrus@gmail.com>
2026-02-19 15:32:42 -0600

    wowwwwwww so thats like 20x faster

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,31 @@ use std::time::Instant;
 
 const CONVERGENCE_CONDITION: f32 = 1e-4;
 
-fn main() {
-    let n = 8;
-    let mut x = generate_random_matrix(n, n);
-    println!("x {x:?}");
-    let start = Instant::now();
-    let ksvd = RandomizedSvd::new(6, x.clone());
 
-    ksvd.qrl.left_apply_qt(&mut x);
-    x = x.transpose();
-    ksvd.qrr.left_apply_qt(&mut x);
-    x = x.transpose();
-    let tiny = ksvd.approx();
-    let big = ksvd.reconstruct();
-    let svalues = RankKSvd::new(4, x.clone());
+fn main() {
+    let n = 1000;
+    let mut x = generate_random_matrix(n, n);
+    // println!("x {x:?}");
+    let start = Instant::now();
+    for _ in 0..100 {
+        let ksvd = RandomizedSvd::new(20, x.clone());
+
+        ksvd.qrl.left_apply_qt(&mut x);
+        x = x.transpose();
+        ksvd.qrr.left_apply_qt(&mut x);
+        x.transpose();
+        let tiny = ksvd.approx();
+        let big = ksvd.reconstruct();
+        let svalues = RankKSvd::new(4, x.clone());
+    }
     let duration = start.elapsed();
     println!("Pipeline took {:?}", duration / 100);
-    println!("rotated {x:?}");
-    println!("tiny {tiny:?}");
-    println!("big {big:?}");
-    println!("s reference {:?}", ksvd.svd.s);
-    println!("singular values {:?}", svalues.singular);
 
 
+//     println!("rotated {x:?}");
+//     println!("tiny {tiny:?}");
+//     // println!("big {big:?}");
+//     println!("s reference {:?}", ksvd.svd.s);
+//     println!("singular values {:?}", svalues.singular);
 
 }
