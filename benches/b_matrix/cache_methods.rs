c33c4ae0515a200de82fd7e889a10712444814e8
autumn <cyancirrus@gmail.com>
2026-04-06 19:39:17 -0500

    going to revamp

--- benches/b_matrix/cache_methods.rs
+++ benches/b_matrix/cache_methods.rs
@@ -1,18 +1,47 @@
 use crate::sharedvars::{L_MATRIX_DIMS, M_MATRIX_DIMS, S_MATRIX_DIMS};
-use criterion::{BenchmarkId, Criterion, black_box};
+use criterion::{BenchmarkId, Criterion, Throughput, black_box};
 use stellar::algebra::mmethods::{par_tensor_mult_cache, tensor_mult_cache};
-// use stellar::algebra::ndmethods::{tensor_mult, basic_mult};
+use stellar::algebra::ndmethods::{basic_mult, tensor_mult};
 use stellar::random::generation::generate_random_matrix;
 
 const BLOCK: usize = 8;
 pub fn bench_small_matmul(c: &mut Criterion) {
-    let mut group = c.benchmark_group("MatMulImplementation");
+    let mut group = c.benchmark_group("Small MatMulImplementation");
     let bsize = BLOCK * BLOCK;
 
     for &(i, k, j) in S_MATRIX_DIMS.iter() {
-        let parameter = format!("{} * {} * {}", i, k, j);
+        let parameter = format!("{}x{}x{}", i, k, j);
+        group.throughput(Throughput::Elements((i * k * j) as u64));
         group.bench_with_input(
-            BenchmarkId::new("Small Tensor Mult Cache", &parameter),
+            BenchmarkId::new("naive", &parameter),
+            &(i, j, k),
+            |b, &(i, j, k)| {
+                b.iter_with_setup(
+                    || {
+                        let x = generate_random_matrix(i, k);
+                        let y = generate_random_matrix(k, j);
+                        (x, y)
+                    },
+                    |(x, y)| black_box(tensor_mult(BLOCK, &x, &y)),
+                );
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("block", &parameter),
+            &(i, j, k),
+            |b, &(i, j, k)| {
+                b.iter_with_setup(
+                    || {
+                        let x = generate_random_matrix(i, k);
+                        let y = generate_random_matrix(k, j);
+                        (x, y)
+                    },
+                    |(x, y)| black_box(basic_mult(&x, &y)),
+                );
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("cache", &parameter),
             &(i, j, k),
             |b, &(i, j, k)| {
                 b.iter_with_setup(
@@ -38,7 +67,7 @@ pub fn bench_small_matmul(c: &mut Criterion) {
             },
         );
         group.bench_with_input(
-            BenchmarkId::new("Small Par Tensor Mult Cache", &parameter),
+            BenchmarkId::new("parcache", &parameter),
             &(i, j, k),
             |b, &(i, j, k)| {
                 b.iter_with_setup(
@@ -59,13 +88,42 @@ pub fn bench_small_matmul(c: &mut Criterion) {
 }
 
 pub fn bench_medium_matmul(c: &mut Criterion) {
-    let mut group = c.benchmark_group("MatMulImplementation");
+    let mut group = c.benchmark_group("Medium MatMulImplementation");
     let bsize = BLOCK * BLOCK;
 
     for &(i, k, j) in M_MATRIX_DIMS.iter() {
-        let parameter = format!("{} * {} * {}", i, k, j);
+        let parameter = format!("{}x{}x{}", i, k, j);
+        group.throughput(Throughput::Elements((i * k * j) as u64));
         group.bench_with_input(
-            BenchmarkId::new("Medium Tensor Mult Cache", &parameter),
+            BenchmarkId::new("naive", &parameter),
+            &(i, j, k),
+            |b, &(i, j, k)| {
+                b.iter_with_setup(
+                    || {
+                        let x = generate_random_matrix(i, k);
+                        let y = generate_random_matrix(k, j);
+                        (x, y)
+                    },
+                    |(x, y)| black_box(tensor_mult(BLOCK, &x, &y)),
+                );
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("block", &parameter),
+            &(i, j, k),
+            |b, &(i, j, k)| {
+                b.iter_with_setup(
+                    || {
+                        let x = generate_random_matrix(i, k);
+                        let y = generate_random_matrix(k, j);
+                        (x, y)
+                    },
+                    |(x, y)| black_box(basic_mult(&x, &y)),
+                );
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("cache", &parameter),
             &(i, j, k),
             |b, &(i, j, k)| {
                 b.iter_with_setup(
@@ -88,23 +146,6 @@ pub fn bench_medium_matmul(c: &mut Criterion) {
                         ))
                     },
                 );
-            },
-        );
-        group.bench_with_input(
-            BenchmarkId::new("Medium Par Tensor Mult Cache", &parameter),
-            &(i, j, k),
-            |b, &(i, j, k)| {
-                b.iter_with_setup(
-                    || {
-                        let x = generate_random_matrix(i, k);
-                        let y = generate_random_matrix(k, j);
-                        let target = vec![f32::NAN; i * j];
-                        (x, y, target)
-                    },
-                    |(x, y, mut target)| {
-                        black_box(par_tensor_mult_cache(&x, &y, &mut target, BLOCK))
-                    },
-                )
             },
         );
     }
@@ -112,13 +153,48 @@ pub fn bench_medium_matmul(c: &mut Criterion) {
 }
 
 pub fn bench_large_matmul(c: &mut Criterion) {
-    let mut group = c.benchmark_group("MatMulImplementation");
+    let mut group = c.benchmark_group("Large MatMulImplementation");
     let bsize = BLOCK * BLOCK;
 
     for &(i, k, j) in L_MATRIX_DIMS.iter() {
-        let parameter = format!("{} * {} * {}", i, k, j);
+        let parameter = format!("{}x{}x{}", i, k, j);
+        group.throughput(Throughput::Elements((i * k * j) as u64));
+        // group.bench_with_input(
+        //     BenchmarkId::new("naive", &parameter),
+        //     &(i, j, k),
+        //     |b, &(i, j, k)| {
+        //         b.iter_with_setup(
+        //             || {
+        //                 let x = generate_random_matrix(i, k);
+        //                 let y = generate_random_matrix(k, j);
+        //                 (x, y)
+        //             },
+        //             |(x, y)| {
+        //                 black_box(tensor_mult(
+        //                     BLOCK,
+        //                     &x,
+        //                     &y,
+        //                 ))
+        //             },
+        //         );
+        //     },
+        // );
         group.bench_with_input(
-            BenchmarkId::new("Large Tensor Mult Cache", &parameter),
+            BenchmarkId::new("block", &parameter),
+            &(i, j, k),
+            |b, &(i, j, k)| {
+                b.iter_with_setup(
+                    || {
+                        let x = generate_random_matrix(i, k);
+                        let y = generate_random_matrix(k, j);
+                        (x, y)
+                    },
+                    |(x, y)| black_box(basic_mult(&x, &y)),
+                );
+            },
+        );
+        group.bench_with_input(
+            BenchmarkId::new("cache", &parameter),
             &(i, j, k),
             |b, &(i, j, k)| {
                 b.iter_with_setup(
@@ -141,23 +217,6 @@ pub fn bench_large_matmul(c: &mut Criterion) {
                         ))
                     },
                 );
-            },
-        );
-        group.bench_with_input(
-            BenchmarkId::new("Large Par Tensor Mult Cache", &parameter),
-            &(i, j, k),
-            |b, &(i, j, k)| {
-                b.iter_with_setup(
-                    || {
-                        let x = generate_random_matrix(i, k);
-                        let y = generate_random_matrix(k, j);
-                        let target = vec![f32::NAN; i * j];
-                        (x, y, target)
-                    },
-                    |(x, y, mut target)| {
-                        black_box(par_tensor_mult_cache(&x, &y, &mut target, BLOCK))
-                    },
-                )
             },
         );
     }
--- benches/matrix.rs
+++ benches/matrix.rs
@@ -7,6 +7,6 @@ criterion_group!(
     benches_performance,
     cache_methods::bench_small_matmul,
     cache_methods::bench_medium_matmul,
-    cache_methods::bench_large_matmul,
+    // cache_methods::bench_large_matmul,
 );
 criterion_main!(benches_performance);
