f186956bad477de5117a86c441b86a20666a33af
autumn <cyancirrus@gmail.com>
2026-04-15 09:44:41 -0500

    doing things

--- scripts/flame_matrix_benchmark.sh
+++ scripts/flame_matrix_benchmark.sh
@@ -2,5 +2,6 @@
 echo "Running Matrix Benchmark"
 cd "$(dirname "$0")/.."
 
-RAYON_NUM_THREADS=4 cargo criterion --bench matrix_flame --features="avx2"
+# RAYON_NUM_THREADS=4 cargo criterion --bench matrix_flame --features="avx2"
+RAYON_NUM_THREADS=4 cargo samply --bench matrix_flame --features="avx2"
 chromium  "./diagnostics/flamegraph_matmul.svg"
