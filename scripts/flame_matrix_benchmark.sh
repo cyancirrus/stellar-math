1fb621148d245632272c59a3ae8966833b2b7dd2
autumn <cyancirrus@gmail.com>
2026-04-15 09:57:09 -0500

    maybe this

--- scripts/flame_matrix_benchmark.sh
+++ scripts/flame_matrix_benchmark.sh
@@ -4,5 +4,5 @@ cd "$(dirname "$0")/.."
 
 # RAYON_NUM_THREADS=4 cargo criterion --bench matrix_flame --features="avx2"
 RAYON_NUM_THREADS=4 cargo samply --bench matrix_flame --features="avx2"
-# chromium  "./diagnostics/flamegraph_matmul.svg"
-chromium http://localhost:3000
+chromium  "./diagnostics/flamegraph_matmul.svg"
+# chromium http://localhost:3000
