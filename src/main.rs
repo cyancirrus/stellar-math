b7b44618cf354025d8b19ccd07073ff13e4230c4
autumn <cyancirrus@gmail.com>
2026-04-30 17:54:34 -0500

    saving before implementing

--- src/main.rs
+++ src/main.rs
@@ -19,48 +19,5 @@ use stellar::algebra::ndmethods::basic_mult;
 use stellar::equality::approximate::approx_vector_eq;
 use stellar::random::generation::generate_random_matrix;
 use stellar::structure::ndarray::NdArray;
-// #[cfg(all(feature = "avx2", target_arch = "x86_64"))]
-use std::arch::x86_64::{
-    __m256, __m256i, _MM_HINT_T0, _mm_prefetch, _mm256_add_ps, _mm256_and_ps, _mm256_broadcast_ss,
-    _mm256_castsi256_ps, _mm256_fmadd_ps, _mm256_loadu_si256, _mm256_maskload_ps,
-    _mm256_maskstore_ps, _mm256_set1_epi32, _mm256_setzero_ps,
-};
-
-// negative 1 is twos complement so all bits active
-#[rustfmt::skip]
-const MASK:[[i32;8];9] = [
-    [ 0,  0,  0,  0,  0,  0,  0,  0],
-    [-1,  0,  0,  0,  0,  0,  0,  0],
-    [-1, -1,  0,  0,  0,  0,  0,  0],
-    [-1, -1, -1,  0,  0,  0,  0,  0],
-    [-1, -1, -1, -1,  0,  0,  0,  0],
-    [-1, -1, -1, -1, -1,  0,  0,  0],
-    [-1, -1, -1, -1, -1, -1,  0,  0],
-    [-1, -1, -1, -1, -1, -1, -1,  0],
-    [-1, -1, -1, -1, -1, -1, -1, -1],
-];
-
-static ZEROS: [f32; 8] = [0f32; 8];
-unsafe fn gate_row(ptr: *const f32, ctrl: i32, mask: __m256i) -> __m256 {
-    unsafe {
-        let safe_ptr = if ctrl != 0 { ptr } else { ZEROS.as_ptr() };
-        _mm256_maskload_ps(safe_ptr, mask)
-    }
-}
-unsafe fn sgate_row(ptr: *mut f32, ctrl: i32, mask: __m256i, data: __m256) {
-    unsafe {
-        if ctrl != 0 {
-            _mm256_maskstore_ps(ptr, mask, data);
-        }
-    }
-}
-macro_rules! fma_gated {
-    ($acc:expr, $ptr:expr, $mask_bit:expr, $data:expr) => {
-        if $mask_bit != 0 {
-            $acc = _mm256_fmadd_ps(_mm256_broadcast_ss(&*$ptr), $data, $acc);
-            // $acc = _mm256_fmadd_ps(_mm256_set1_ps(*$ptr), $b, $acc);
-        }
-    };
-}
 
 fn main() {}
