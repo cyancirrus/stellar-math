39a20535b67ca3dd026f9cfc3d95946388794cce
autumn <cyancirrus@gmail.com>
2026-05-26 13:22:21 -0500

    yay kernel working

--- src/main.rs
+++ src/main.rs
@@ -14,8 +14,6 @@
 // 3. Triangle kernel     ← 2hrs, unblocks LQ block
 // 4. Trait refactor      ← important but least urgent
 
-use std::ptr::copy_nonoverlapping;
-use stellar::algebra::bmethods::pack;
 use stellar::arch::SIMD_WIDTH;
 #[cfg(all(feature = "avx2", target_arch = "x86_64"))]
 use stellar::kernel::avx2::constants::{MASK, cfma_accum, fma_accum, mask_load, mask_store_ctrl};
@@ -42,10 +40,10 @@ fn default_mult(bptr: *mut f32, dptr: *const f32, re: usize, se: usize, s_b: usi
         println!("recompile with avx2");
     }
 }
-
-pub fn kernel_umult(
+#[inline(always)]
+pub fn kernel_mult_simd(
     mut xptr: *const f32,
-    mut yptr: *mut f32,
+    mut yptr: *const f32,
     tptr: *mut f32,
     m: usize,
     p: usize,
@@ -54,29 +52,16 @@ pub fn kernel_umult(
     s_y: usize,
     s_t: usize,
 ) {
-    #[cfg(all(feature = "avx2", target_arch = "x86_64"))]
-    kernel_mult_unalligned!(xptr, yptr, tptr, m, p, n, s_x, s_y, s_t);
-    #[cfg(not(any(feature = "avx2")))]
-    println!("hey run it with flag for arch");
-    // default_mult(bptr, dptr, re, se, PC, s_d);
+    // happens over k-contraction needs the imult kernel
+    unsafe {
+        if (m | n) & (SIMD_WIDTH - 1) == 0 {
+            kernel_mult_alligned!(xptr, yptr, tptr, m, p, n, s_x, s_y, s_t);
+        } else {
+            kernel_mult_unalligned!(xptr, yptr, tptr, m, p, n, s_x, s_y, s_t);
+        }
+    }
 }
-// pub fn kernel_mult(
-//     mut xptr: *const f32,
-//     mut yptr: *mut f32,
-//     tptr: *mut f32,
-//     m: usize,
-//     p: usize,
-//     n: usize,
-//     s_x: usize,
-//     s_y: usize,
-//     s_t: usize,
-// ) {
-//     #[cfg(all(feature = "avx2", target_arch = "x86_64"))]
-//     kernel_mult_alligned!(xptr, yptr, tptr, m, p, n, s_x, s_y, s_t);
-//     #[cfg(not(any(feature = "avx2")))]
-//     println!("hey run it with flag for arch");
-//     // default_mult(bptr, dptr, re, se, PC, s_d);
-// }
+
 // pub fn kernel_lmult_lt(
 //     mut xptr: *const f32,
 //     mut yptr: *mut f32,
