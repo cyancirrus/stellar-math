18286217cdfd09cda68c9d6d7c277ab43926119a
autumn <cyancirrus@gmail.com>
2026-06-07 10:51:57 -0500

    better unified interface

--- src/kernel/avx2/interface.rs
+++ src/kernel/avx2/interface.rs
@@ -34,17 +34,17 @@ pub fn kernel_lt_mult_simd(
     s_y: usize,
     s_t: usize,
 ) {
-    let pre = d_sub.saturating_sub(d_add);
-    let pro = d_add.saturating_sub(d_sub);
-    let pos = if d_sub > d_add {
-        m - pre
-    } else if d_add < p + d_sub {
-        (p.wrapping_sub(pro)).min(SIMD_WIDTH)
-    } else {
-        0
-    };
+    let d_pos = d_add.saturating_sub(d_sub);
+    let d_neg = d_sub.saturating_sub(d_add);
+    // preprocess the diagonal when on boundary 
+    let pre = d_neg;
+    // process the diagonal when occurred in range 
+    let pro = d_pos;
+    // process the dense part
+    debug_assert!(m >= d_neg, "m {m:}, d_neg: {d_neg:}");
+    let pos = (p - p.min(d_pos)).min(m - d_neg);
     unsafe {
-        if pos > 0 {
+        if pos != 0 {
             ltriangle::lmult_lt(xptr, yptr, tptr, pre, pro, pos, m, p, n, s_x, s_y, s_t);
         } else {
             kernel_mult_simd(xptr, yptr, tptr, m, p, n, s_x, s_y, s_t);
@@ -64,17 +64,14 @@ pub fn kernel_ut_mult_simd(
     s_y: usize,
     s_t: usize,
 ) {
-    let (d_pos, d_neg) = if d_add > d_sub {
-        (d_add - d_sub, 0 )
-    } else {
-        (0, d_sub - d_add)
-    };
+    let d_pos = d_add.saturating_sub(d_sub);
+    let d_neg = d_sub.saturating_sub(d_add);
     // pre-allign left boundary point
     let pre = m.min(d_neg);
     // handle triangle part of upper triangular
     let pos = (p - p.min(d_pos)).min(m - pre);
     // process the dense part
-    // debug_assert!(d_pos + pos <= p, "d_pos:{d_pos} pos:{pos} p:{p}");
+    debug_assert!(d_pos + pos <= p, "d_pos: {d_pos}, pos:{pos}, p:{p}");
     let pro = p - d_pos - pos;
     unsafe {
         p = p - d_pos;
@@ -83,39 +80,10 @@ pub fn kernel_ut_mult_simd(
         // index down for target row of y for outer product
         yptr = yptr.add(d_pos * s_y);
         // tptr is constant ie target output vectors are fixed
-        if pos > 0 {
+        if pos != 0 {
             ltriangle::lmult_ut(xptr, yptr, tptr, pre, pro, pos, m, p, n, s_x, s_y, s_t);
         } else {
             kernel_mult_simd(xptr, yptr, tptr, m, p, n, s_x, s_y, s_t);
         }
     }
 }
-// pub fn kernel_lt_mult_simd(
-//     xptr: *const f32,
-//     yptr: *const f32,
-//     tptr: *mut f32,
-//     d: isize,
-//     m: usize,
-//     p: usize,
-//     n: usize,
-//     s_x: usize,
-//     s_y: usize,
-//     s_t: usize,
-// ) {
-//     let pre = (-d).max(0) as usize;
-//     let pro = d.max(0) as usize;
-//     let pos = if d <= 0 {
-//         m - pre
-//     } else if d < p as isize {
-//         (p.wrapping_sub(pro)).min(SIMD_WIDTH)
-//     } else {
-//         0
-//     };
-//     unsafe {
-//         if pos > 0 {
-//             ltriangle::lmult_lt(xptr, yptr, tptr, pre, pro, pos, m, p, n, s_x, s_y, s_t);
-//         } else {
-//             kernel_mult_simd(xptr, yptr, tptr, m, p, n, s_x, s_y, s_t);
-//         }
-//     }
-// }
