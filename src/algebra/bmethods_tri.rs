da3680dd447114a990b9bb3b59f3d807f5edbdde
autumn <cyancirrus@gmail.com>
2026-06-22 11:24:14 -0500

    integration last one

--- src/algebra/bmethods_tri.rs
+++ src/algebra/bmethods_tri.rs
@@ -1,6 +1,6 @@
 use crate::algebra::bmethods::{diff_min, pack};
 use crate::arch::SIMD_WIDTH;
-use crate::kernel::matkerns::{kernel_lt_mult, kernel_rlt_mult, kernel_rut_mult, kernel_ut_mult};
+use crate::kernel::matkerns::{kernel_lt_mult,kernel_tlt_mult,  kernel_rlt_mult, kernel_rut_mult, kernel_ut_mult};
 use rayon::prelude::*;
 use rayon::slice::ParallelSlice;
 use std::cell::RefCell;
@@ -390,6 +390,99 @@ pub fn tensor_rut_contraction(
         }
     }
 }
+pub fn tensor_tlt_block(
+    x_d: &[f32],
+    y_d: &[f32],
+    t_d: &mut [f32],
+    m: usize,
+    p: usize,
+    n: usize,
+    s_x: usize,
+    s_y: usize,
+    s_t: usize,
+) {
+    // diagonal
+    // suffix c: chunk, suffix a: actual
+    let d_add = p - p.min(m) + 1;
+    t_d.par_chunks_mut(MC * n)
+        .enumerate()
+        .for_each(|(mc_idx, t)| {
+            PACK.with(|workspace_cell| {
+                let (x_pack, y_pack, t_accum) = &mut *workspace_cell.borrow_mut();
+                let d_add = d_add + mc_idx * MC;
+                let dy = PC * s_y;
+                let d_xt = PC * s_x;
+                let ma = diff_min(m, mc_idx * MC, MC);
+                // let (xend, tend) = (ma * s_x, ma * s_t);
+                let tend = ma * s_t;
+                for nc in (0..n).step_by(NC) {
+                    let na = diff_min(n, nc, NC);
+                    t_accum.fill(0f32);
+                    // base column offset
+                    let mut xoffset = mc_idx * MC;
+                    let mut yoffset = 0;
+                    for pc in (0..p).step_by(PC) {
+                        let pa = diff_min(p, pc, PC);
+                        // let yend = pa * s_y;
+                        pack(&x_d[xoffset..], x_pack, pa, ma, MC, s_x);
+                        // pack(&y_d[yoffset + nc..yoffset + yend], y_pack, pa, na, NC, s_y);
+                        pack(&y_d[yoffset + nc..], y_pack, pa, na, NC, s_y);
+                        tensor_tlt_contraction(
+                            &x_pack, &y_pack, t_accum, d_add, pc, ma, pa, na, MC, NC, NC,
+                        );
+                        yoffset += dy;
+                        xoffset += d_xt;
+                    }
+                    // unpack
+                    pack(&t_accum, &mut t[nc..tend], ma, na, s_t, NC);
+                }
+            })
+        });
+}
+pub fn tensor_tlt_contraction(
+    x_d: &[f32],
+    y_d: &[f32],
+    t_d: &mut [f32],
+    mut d_add: usize,
+    d_sub: usize,
+    m: usize,
+    p: usize,
+    n: usize,
+    s_x: usize,
+    s_y: usize,
+    s_t: usize,
+) {
+    unsafe {
+        let mut xoffset = 0;
+        let mut toffset = 0;
+        let dx = SIMD_WIDTH;
+        let dt = SIMD_WIDTH * s_t;
+        for i in (0..m).step_by(SIMD_WIDTH) {
+            let ii_end = SIMD_WIDTH.min(m - i);
+            if d_add + ii_end > d_sub {
+                for j in (0..n).step_by(SIMD_WIDTH) {
+                    let jj_end = SIMD_WIDTH.min(n - j);
+                    kernel_tlt_mult(
+                        x_d.get_unchecked(xoffset..),
+                        y_d.get_unchecked(j..),
+                        t_d.get_unchecked_mut(toffset + j..),
+                        d_add,
+                        d_sub,
+                        ii_end,
+                        p,
+                        jj_end,
+                        s_x,
+                        s_y,
+                        s_t,
+                    )
+                }
+            }
+            toffset += dt;
+            xoffset += dx;
+            d_add += SIMD_WIDTH;
+        }
+    }
+}
 #[cfg(test)]
 #[cfg(feature = "avx2")]
 mod test_lower_and_upper_triangular_dispatch {
@@ -454,6 +547,7 @@ mod test_lower_and_upper_triangular_dispatch {
             upper_equivalence_mkn(i, k, j);
             rlower_equivalence_mkn(i, k, j);
             rupper_equivalence_mkn(i, k, j);
+            ltl_equivalence_mkn(i, k, j);
         }
     }
     /// Case 1 / Case 2
@@ -506,7 +600,10 @@ mod test_lower_and_upper_triangular_dispatch {
             dims: vec![m, n],
             data: result.clone(),
         };
-        assert!(approx_vector_eq(&expected.data, &result[..m * n]));
+        assert!(
+            approx_vector_eq(&expected.data, &result[..m * n]),
+            "FAILURE WAS ({m:}, {p:}, {n:})"
+        );
     }
     fn upper_equivalence_mkn(m: usize, p: usize, n: usize) {
         let x = generate_random_matrix(m, p);
@@ -520,7 +617,10 @@ mod test_lower_and_upper_triangular_dispatch {
             dims: vec![m, n],
             data: result.clone(),
         };
-        assert!(approx_vector_eq(&expected.data, &result[..m * n]));
+        assert!(
+            approx_vector_eq(&expected.data, &result[..m * n]),
+            "FAILURE WAS ({m:}, {p:}, {n:})"
+        );
     }
     fn rlower_equivalence_mkn(m: usize, p: usize, n: usize) {
         let x = generate_random_matrix(m, p);
@@ -536,9 +636,10 @@ mod test_lower_and_upper_triangular_dispatch {
             dims: vec![m, n],
             data: result.clone(),
         };
-        // println!("expected {expected:?}");
-        // println!("actual {_inspect:?}");
-        assert!(approx_vector_eq(&expected.data, &result[..m * n]));
+        assert!(
+            approx_vector_eq(&expected.data, &result[..m * n]),
+            "FAILURE WAS ({m:}, {p:}, {n:})"
+        );
     }
     fn rupper_equivalence_mkn(m: usize, p: usize, n: usize) {
         let x = generate_random_matrix(m, p);
@@ -556,6 +657,33 @@ mod test_lower_and_upper_triangular_dispatch {
         };
         // println!("expected {expected:?}");
         // println!("actual {_inspect:?}");
-        assert!(approx_vector_eq(&expected.data, &result[..m * n]));
+        assert!(
+            approx_vector_eq(&expected.data, &result[..m * n]),
+            "FAILURE WAS ({m:}, {p:}, {n:})"
+        );
+    }
+    fn ltl_equivalence_mkn(m: usize, p: usize, n: usize) {
+        let y = generate_random_matrix(p, n);
+        let mut x = generate_random_matrix(m, p);
+        let mut x_base = x.clone();
+        filter_lower_triangle(&mut x_base);
+        x.transpose_inplace(); // stored in transpose
+        // println!("x_base {x_base:?}");
+        // println!("y {y:?}");
+        let expected = basic_mult(&x_base, &y);
+        let mut result = vec![0f32; m * n];
+        // tensor_tlt_block(&x.data, &y.data, &mut result, m, p, n, p, n, n);
+        // m, n, n b/c X is s tored in it's transposed state
+        tensor_tlt_block(&x.data, &y.data, &mut result, m, p, n, m, n, n);
+        let _inspect = NdArray {
+            dims: vec![m, n],
+            data: result.clone(),
+        };
+        // println!("expected {expected:?}");
+        // println!("actual {_inspect:?}");
+        assert!(
+            approx_vector_eq(&expected.data, &result[..m * n]),
+            "FAILURE WAS ({m:}, {p:}, {n:})"
+        );
     }
 }
