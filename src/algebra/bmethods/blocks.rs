d5cffee42632bcba9377785687253e8821dfc4a7
autumn <cyancirrus@gmail.com>
2026-06-22 16:56:31 -0500

    easy api interface for dense matrices

--- src/algebra/bmethods/blocks.rs
+++ src/algebra/bmethods/blocks.rs
@@ -111,6 +111,8 @@ pub fn tensor_lt_block(
     x_d: &[f32],
     y_d: &[f32],
     t_d: &mut [f32],
+    d_add:usize,
+    d_sub:usize,
     m: usize,
     p: usize,
     n: usize,
@@ -120,7 +122,7 @@ pub fn tensor_lt_block(
 ) {
     // diagonal
     // suffix c: chunk, suffix a: actual
-    let d_add = p - p.min(m) + 1;
+    // let d_add = p - p.min(m) + 1;
     t_d.par_chunks_mut(MC * n)
         .zip(x_d.par_chunks(MC * p))
         .enumerate()
@@ -155,6 +157,8 @@ pub fn tensor_ut_block(
     x_d: &[f32],
     y_d: &[f32],
     t_d: &mut [f32],
+    d_add:usize,
+    d_sub:usize,
     m: usize,
     p: usize,
     n: usize,
@@ -163,7 +167,7 @@ pub fn tensor_ut_block(
     s_t: usize,
 ) {
     // suffix c: chunk, suffix a: actual
-    let d_sub = m.saturating_sub(p);
+    // let d_sub = m.saturating_sub(p);
     t_d.par_chunks_mut(MC * n)
         .zip(x_d.par_chunks(MC * p))
         .enumerate()
@@ -208,6 +212,8 @@ pub fn tensor_rlt_block(
     x_d: &[f32],
     y_d: &[f32],
     t_d: &mut [f32],
+    d_add:usize,
+    d_sub:usize,
     _m: usize,
     p: usize,
     n: usize,
@@ -217,7 +223,7 @@ pub fn tensor_rlt_block(
 ) {
     // diagonal
     // suffix c: chunk, suffix a: actual
-    let d_add = n - n.min(p);
+    // let d_add = n - n.min(p);
     t_d.par_chunks_mut(MC * n)
         .zip(x_d.par_chunks(MC * p))
         .for_each(|(t, x)| {
@@ -260,6 +266,8 @@ pub fn tensor_rut_block(
     x_d: &[f32],
     y_d: &[f32],
     t_d: &mut [f32],
+    d_add:usize,
+    d_sub:usize,
     _m: usize,
     p: usize,
     n: usize,
@@ -268,7 +276,7 @@ pub fn tensor_rut_block(
     s_t: usize,
 ) {
     // diagonal
-    let d_add = p - p.min(n) + 1;
+    // let d_add = p - p.min(n) + 1;
     t_d.par_chunks_mut(MC * n)
         .zip(x_d.par_chunks(MC * p))
         .for_each(|(t, x)| {
@@ -311,6 +319,8 @@ pub fn tensor_tlt_block(
     x_d: &[f32],
     y_d: &[f32],
     t_d: &mut [f32],
+    d_add:usize,
+    d_sub:usize,
     m: usize,
     p: usize,
     n: usize,
@@ -320,7 +330,7 @@ pub fn tensor_tlt_block(
 ) {
     // diagonal
     // suffix c: chunk, suffix a: actual
-    let d_add = p - p.min(m) + 1;
+    // let d_add = p - p.min(m) + 1;
     t_d.par_chunks_mut(MC * n)
         .enumerate()
         .for_each(|(mc_idx, t)| {
@@ -360,6 +370,8 @@ pub fn tensor_tut_block(
     x_d: &[f32],
     y_d: &[f32],
     t_d: &mut [f32],
+    d_add:usize,
+    d_sub:usize,
     m: usize,
     p: usize,
     n: usize,
@@ -369,7 +381,7 @@ pub fn tensor_tut_block(
 ) {
     // diagonal
     // suffix c: chunk, suffix a: actual
-    let d_sub = m.saturating_sub(p);
+    // let d_sub = m.saturating_sub(p);
     t_d.par_chunks_mut(MC * n)
         .enumerate()
         .for_each(|(mc_idx, t)| {
@@ -411,358 +423,4 @@ pub fn tensor_tut_block(
                 }
             })
         });
-}
-
-#[cfg(test)]
-mod test_kernel_block {
-    use super::*;
-    use crate::algebra::ndmethods::basic_mult;
-    use crate::arch::SIMD_WIDTH;
-    use crate::equality::approximate::approx_vector_eq;
-    use crate::random::generation::generate_random_matrix;
-    use crate::structure::ndarray::NdArray;
-
-    #[test]
-    fn test_outkern_equivalence() {
-        let ikj = [
-            (1, 1, 1),
-            (8, 1, 1),
-            (1, 8, 1),
-            (1, 1, 8),
-            (6, 4, 8),
-            (6, 8, 4),
-            (4, 6, 8),
-            (4, 8, 6),
-            (8, 4, 6),
-            (8, 6, 4),
-            (8, 8, 8),
-            (16, 16, 16),
-            (32, 32, 32),
-            (64, 64, 64),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH + 1, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH, SIMD_WIDTH + 1, SIMD_WIDTH),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH + 1),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH - 1, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH, SIMD_WIDTH - 1, SIMD_WIDTH),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH - 1),
-        ];
-        for (i, k, j) in ikj {
-            test_outkern_equivalence_mkn(i, k, j);
-        }
-    }
-    fn test_outkern_equivalence_mkn(m: usize, p: usize, n: usize) {
-        let x = generate_random_matrix(m, p);
-        let y = generate_random_matrix(p, n);
-        let mut result = vec![0f32; m * n];
-        let expected = basic_mult(&x, &y);
-        tensor_contraction(&x.data, &y.data, &mut result, m, p, n, p, n, n);
-        // let inspect = NdArray {
-        //     dims: vec![m, n],
-        //     data: result.clone(),
-        // };
-        // println!("expected {expected:?}");
-        // println!("actual {inspect:?}");
-        assert!(approx_vector_eq(&expected.data, &result[..m * n]));
-    }
-    #[test]
-    fn test_gemm_equivalence() {
-        let ikj = [
-            // (256, 256, 256),
-            (SIMD_WIDTH, SIMD_WIDTH + 1, SIMD_WIDTH),
-            (1, 1, 1),
-            (8, 1, 1),
-            (1, 8, 1),
-            (1, 1, 8),
-            (6, 4, 8),
-            (6, 8, 4),
-            (4, 6, 8),
-            (4, 8, 6),
-            (8, 4, 6),
-            (8, 6, 4),
-            (8, 8, 8),
-            (16, 16, 16),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH + 1, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH + 1),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH - 1, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH, SIMD_WIDTH - 1, SIMD_WIDTH),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH - 1),
-            (256, 1024, 512),
-            (512, 512, 512),
-            (1024, 64, 1024),
-        ];
-        for (i, k, j) in ikj {
-            println!("(i: {i:?}, k: {k:?}, j: {j:})");
-            matmul_equivalence(i, k, j);
-            tmatmul_equivalence(i, k, j);
-        }
-    }
-    fn matmul_equivalence(m: usize, k: usize, n: usize) {
-        let x = generate_random_matrix(m, k);
-        let y = generate_random_matrix(k, n);
-        let mut result = vec![0f32; m * n];
-        let expected = basic_mult(&x, &y);
-        tensor_block(&x.data, &y.data, &mut result, m, k, n, k, n, n);
-        let inspect = NdArray {
-            dims: vec![m, n],
-            data: result.clone(),
-        };
-        println!("expected {expected:?}");
-        println!("actual {inspect:?}");
-        assert!(approx_vector_eq(&expected.data, &result[..m * n]));
-    }
-    fn tmatmul_equivalence(m: usize, p: usize, n: usize) {
-        let y = generate_random_matrix(p, n);
-        let mut x = generate_random_matrix(m, p);
-        let mut x_base = x.clone();
-        x.transpose_inplace();
-        // println!("x_base {x_base:?}");
-        // println!("y {y:?}");
-        let expected = basic_mult(&x_base, &y);
-        let mut result = vec![0f32; m * n];
-        // m, n, n b/c X is s tored in it's transposed state
-        tensor_tblock(&x.data, &y.data, &mut result, m, p, n, m, n, n);
-        let _inspect = NdArray {
-            dims: vec![m, n],
-            data: result.clone(),
-        };
-        // println!("expected {expected:?}");
-        // println!("actual {_inspect:?}");
-        assert!(
-            approx_vector_eq(&expected.data, &result[..m * n]),
-            "FAILURE WAS ({m:}, {p:}, {n:})"
-        );
-    }
-}
-#[cfg(test)]
-#[cfg(feature = "avx2")]
-mod test_lower_and_upper_triangular_dispatch {
-    use super::*;
-    use crate::algebra::ndmethods::basic_mult;
-    use crate::equality::approximate::approx_vector_eq;
-    use crate::random::generation::generate_random_matrix;
-    use crate::structure::ndarray::NdArray;
-    #[test]
-    fn test_gemm_equivalence() {
-        let ikj = [
-            (9, 16, 9),
-            (32, 32, 32),
-            (1, 1, 1),
-            (16, 16, 16),
-            (8, 9, 8),
-            (3, 9, 1),
-            (6, 4, 8),
-            (9, 16, 8),
-            (8, 8, 9),
-            (2, 9, 1),
-            (2, 2, 1),
-            (2, 9, 1),
-            (2, 10, 1),
-            (1, 9, 1),
-            (4, 8, 1),
-            (1, 2, 1),
-            (1, 1, 1),
-            (8, 1, 1),
-            (1, 8, 1),
-            (1, 1, 8),
-            (6, 4, 8),
-            (6, 8, 4),
-            (8, 4, 6),
-            (4, 8, 6),
-            (4, 6, 8),
-            (8, 6, 4),
-            (8, 8, 8),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH + 1, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH, SIMD_WIDTH + 1, SIMD_WIDTH),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH + 1),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH - 1, SIMD_WIDTH, SIMD_WIDTH),
-            (SIMD_WIDTH, SIMD_WIDTH - 1, SIMD_WIDTH),
-            (SIMD_WIDTH, SIMD_WIDTH, SIMD_WIDTH - 1),
-            (MC + 1, PC, NC + 1),
-            (MC + 1, PC, NC - 1),
-            (MC + 1, PC, NC),
-            (MC - 1, PC, NC),
-            (MC, PC + 1, NC),
-            (MC, PC - 1, NC),
-            (MC, PC, NC),
-            (256, 256, 256),
-            (256, 1024, 512),
-            (512, 512, 512),
-            (1024, 64, 1024),
-        ];
-        for (i, k, j) in ikj {
-            println!("(i: {i:?}, k: {k:?}, j: {j:})");
-            lower_equivalence_mkn(i, k, j);
-            upper_equivalence_mkn(i, k, j);
-            rlower_equivalence_mkn(i, k, j);
-            rupper_equivalence_mkn(i, k, j);
-            ltl_equivalence_mkn(i, k, j);
-            ltu_equivalence_mkn(i, k, j);
-        }
-    }
-    /// Case 1 / Case 2
-    /// -------+---------
-    /// * * *  / * * * *
-    /// * * *  / 0 * * *
-    /// 0 * *  /
-    /// 0 0 *  /
-    fn filter_upper_trapezoid(a: &mut NdArray) {
-        // i - (m - n);
-        let (m, n) = (a.dims[0], a.dims[1]);
-        let data = &mut a.data;
-        let mut d_sub = if m > n { m - n } else { 0 };
-        let mut d_plus = 0;
-        for i in 0..m {
-            for j in 0..n {
-                if j + d_sub >= d_plus {
-                    break;
-                }
-                data[i * n + j] = 0f32;
-            }
-            d_plus += 1;
-        }
-    }
-    /// * * * * * * * 0 0 0
-    /// * * * * * * * * 0 0
-    /// * * * * * * * * * 0
-    /// * * * * * * * * * *
-    fn filter_lower_trapezoid(a: &mut NdArray) {
-        let (rows, cols) = (a.dims[0], a.dims[1]);
-        let d = &mut a.data;
-        let t = cols.min(rows);
-        let s = rows.saturating_sub(cols);
-        // don't remove from last row
-        for i in 1..t {
-            for j in 0..i {
-                d[(rows - i - s) * cols - j - 1] = 0f32;
-            }
-        }
-    }
-    fn lower_equivalence_mkn(m: usize, p: usize, n: usize) {
-        let x = generate_random_matrix(m, p);
-        let y = generate_random_matrix(p, n);
-        let mut x_base = x.clone();
-        filter_lower_trapezoid(&mut x_base);
-        let expected = basic_mult(&x_base, &y);
-        let mut result = vec![0f32; m * n];
-        tensor_lt_block(&x.data, &y.data, &mut result, m, p, n, p, n, n);
-        let _inspect = NdArray {
-            dims: vec![m, n],
-            data: result.clone(),
-        };
-        assert!(
-            approx_vector_eq(&expected.data, &result[..m * n]),
-            "FAILURE WAS ({m:}, {p:}, {n:})"
-        );
-    }
-    fn upper_equivalence_mkn(m: usize, p: usize, n: usize) {
-        let x = generate_random_matrix(m, p);
-        let y = generate_random_matrix(p, n);
-        let mut x_base = x.clone();
-        filter_upper_trapezoid(&mut x_base);
-        let expected = basic_mult(&x_base, &y);
-        let mut result = vec![0f32; m * n];
-        tensor_ut_block(&x.data, &y.data, &mut result, m, p, n, p, n, n);
-        let _inspect = NdArray {
-            dims: vec![m, n],
-            data: result.clone(),
-        };
-        assert!(
-            approx_vector_eq(&expected.data, &result[..m * n]),
-            "FAILURE WAS ({m:}, {p:}, {n:})"
-        );
-    }
-    fn rlower_equivalence_mkn(m: usize, p: usize, n: usize) {
-        let x = generate_random_matrix(m, p);
-        let y = generate_random_matrix(p, n);
-        let mut y_base = y.clone();
-        filter_lower_trapezoid(&mut y_base);
-        // println!("x_base {x:?}");
-        // println!("y_base {y_base:?}");
-        let expected = basic_mult(&x, &y_base);
-        let mut result = vec![0f32; m * n];
-        tensor_rlt_block(&x.data, &y.data, &mut result, m, p, n, p, n, n);
-        let _inspect = NdArray {
-            dims: vec![m, n],
-            data: result.clone(),
-        };
-        assert!(
-            approx_vector_eq(&expected.data, &result[..m * n]),
-            "FAILURE WAS ({m:}, {p:}, {n:})"
-        );
-    }
-    fn rupper_equivalence_mkn(m: usize, p: usize, n: usize) {
-        let x = generate_random_matrix(m, p);
-        let y = generate_random_matrix(p, n);
-        let mut y_base = y.clone();
-        filter_upper_trapezoid(&mut y_base);
-        // println!("x_base {x:?}");
-        // println!("y_base {y_base:?}");
-        let expected = basic_mult(&x, &y_base);
-        let mut result = vec![0f32; m * n];
-        tensor_rut_block(&x.data, &y.data, &mut result, m, p, n, p, n, n);
-        let _inspect = NdArray {
-            dims: vec![m, n],
-            data: result.clone(),
-        };
-        // println!("expected {expected:?}");
-        // println!("actual {_inspect:?}");
-        assert!(
-            approx_vector_eq(&expected.data, &result[..m * n]),
-            "FAILURE WAS ({m:}, {p:}, {n:})"
-        );
-    }
-    fn ltl_equivalence_mkn(m: usize, p: usize, n: usize) {
-        let y = generate_random_matrix(p, n);
-        let mut x = generate_random_matrix(m, p);
-        let mut x_base = x.clone();
-        filter_lower_trapezoid(&mut x_base);
-        x.transpose_inplace(); // stored in transpose
-        // println!("x_base {x_base:?}");
-        // println!("y {y:?}");
-        let expected = basic_mult(&x_base, &y);
-        let mut result = vec![0f32; m * n];
-        // tensor_tlt_block(&x.data, &y.data, &mut result, m, p, n, p, n, n);
-        // m, n, n b/c X is s tored in it's transposed state
-        tensor_tlt_block(&x.data, &y.data, &mut result, m, p, n, m, n, n);
-        let _inspect = NdArray {
-            dims: vec![m, n],
-            data: result.clone(),
-        };
-        // println!("expected {expected:?}");
-        // println!("actual {_inspect:?}");
-        assert!(
-            approx_vector_eq(&expected.data, &result[..m * n]),
-            "FAILURE WAS ({m:}, {p:}, {n:})"
-        );
-    }
-    fn ltu_equivalence_mkn(m: usize, p: usize, n: usize) {
-        let y = generate_random_matrix(p, n);
-        let mut x = generate_random_matrix(m, p);
-        let mut x_base = x.clone();
-        // filter_lower_trapezoid(&mut x_base);
-        filter_upper_trapezoid(&mut x_base);
-        x.transpose_inplace(); // stored in transpose
-        // println!("x_base {x_base:?}");
-        // println!("y {y:?}");
-        let expected = basic_mult(&x_base, &y);
-        let mut result = vec![0f32; m * n];
-        // m, n, n b/c X is s tored in it's transposed state
-        tensor_tut_block(&x.data, &y.data, &mut result, m, p, n, m, n, n);
-        let _inspect = NdArray {
-            dims: vec![m, n],
-            data: result.clone(),
-        };
-        // println!("expected {expected:?}");
-        // println!("actual {_inspect:?}");
-        assert!(
-            approx_vector_eq(&expected.data, &result[..m * n]),
-            "FAILURE WAS ({m:}, {p:}, {n:})"
-        );
-    }
 }
