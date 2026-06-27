37417f70341b2fd938dff3e842c97d6f49d8d9f6
autumn <cyancirrus@gmail.com>
2026-06-26 20:30:53 -0500

    strides and fma now live don\'t need stride for s_t at this moment but those should work just didn\'t want to work on items not directly relevant to features, fma kernals donee

--- src/algebra/bmethods/tests/stride.rs
+++ src/algebra/bmethods/tests/stride.rs
@@ -1,11 +1,11 @@
 #[cfg(test)]
 #[cfg(feature = "avx2")]
 mod test_stride_behavior {
-    use crate::algebra::bmethods::interface::*;
     use crate::algebra::bmethods::diagonals::*;
+    use crate::algebra::bmethods::interface::*;
     use crate::algebra::bmethods::tests::helpers::*;
     use crate::algebra::ndmethods::basic_mult;
-    use crate::equality::approximate::{approx_vector_eq};
+    use crate::equality::approximate::approx_vector_eq;
     use crate::random::generation::{generate_random_matrix, generate_random_vector};
 
     #[test]
@@ -36,22 +36,18 @@ mod test_stride_behavior {
         let y = generate_random_matrix(p, n);
         let mut t_d = generate_random_vector(m * n);
         let mut expected = basic_mult(&x, &y);
-
         let mut x_stride = vec![0f32; m * s_x];
         let mut y_stride = vec![0f32; p * s_y];
         increment(&mut expected.data, &t_d, m, n, n, n);
-
         pack_stride(&mut x_stride, &x.data, m, p, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
         stride_kernel(&x_stride, &y_stride, &mut t_d, m, p, n, s_x, s_y, s_t);
-        // tensor_kernel(&x_stride, &y_stride, &mut t_d);
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
         );
     }
     fn sfma_tmatmul_equivalence(m: usize, p: usize, n: usize, s_x: usize, s_y: usize, s_t: usize) {
-        // m, n, n b/c X is s tored in it's transposed state
         let y = generate_random_matrix(p, n);
         let mut x = generate_random_matrix(m, p);
         let x_base = x.clone();
@@ -64,7 +60,6 @@ mod test_stride_behavior {
         pack_stride(&mut x_stride, &x.data, p, m, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
         stride_tkernel(&x_stride, &y_stride, &mut t_d, m, p, n, s_x, s_y, s_t);
-        // tensor_tkernel(&x, &y, &mut t_d);
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
@@ -80,21 +75,19 @@ mod test_stride_behavior {
         filter_lower_trapezoid(&mut x_base);
         let mut expected = basic_mult(&x_base, &y);
         increment(&mut expected.data, &t_d, m, n, n, n);
-        
+
         let (d_add, d_sub) = diagonal_lt(m, p, n);
         pack_stride(&mut x_stride, &x.data, m, p, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
         stride_lt_kernel(
             &x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t,
         );
-        // stride_lt_kernel(&x.data, &y.data, &mut t_d, m, p, n, s_x, s_y, s_t);
-        // tensor_lt_kernel(&x, &y, &mut t_d);
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
         );
     }
-    fn sfma_upper_equivalence(m: usize, p: usize, n: usize, s_x:usize, s_y:usize, s_t:usize) {
+    fn sfma_upper_equivalence(m: usize, p: usize, n: usize, s_x: usize, s_y: usize, s_t: usize) {
         let x = generate_random_matrix(m, p);
         let y = generate_random_matrix(p, n);
         let mut t_d = generate_random_vector(m * n);
@@ -102,20 +95,20 @@ mod test_stride_behavior {
         filter_upper_trapezoid(&mut x_base);
         let mut expected = basic_mult(&x_base, &y);
         increment(&mut expected.data, &t_d, m, n, n, n);
-        // tensor_ut_block(&x.data, &y.data, &mut t_d, m, p, n, p, n, n);
-        // tensor_ut_kernel(&x, &y, &mut t_d);
         let mut x_stride = vec![0f32; m * s_x];
         let mut y_stride = vec![0f32; p * s_y];
         let (d_add, d_sub) = diagonal_ut(m, p, n);
         pack_stride(&mut x_stride, &x.data, m, p, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
-        stride_ut_kernel(&x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
+        stride_ut_kernel(
+            &x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t,
+        );
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
         );
     }
-    fn sfma_rlower_equivalence(m: usize, p: usize, n: usize, s_x:usize, s_y:usize, s_t:usize) {
+    fn sfma_rlower_equivalence(m: usize, p: usize, n: usize, s_x: usize, s_y: usize, s_t: usize) {
         let x = generate_random_matrix(m, p);
         let y = generate_random_matrix(p, n);
         let mut t_d = generate_random_vector(m * n);
@@ -123,20 +116,20 @@ mod test_stride_behavior {
         filter_lower_trapezoid(&mut y_base);
         let mut expected = basic_mult(&x, &y_base);
         increment(&mut expected.data, &t_d, m, n, n, n);
-        // tensor_rlt_block(&x.data, &y.data, &mut t_d, m, p, n, p, n, n);
-        // tensor_rlt_kernel(&x, &y, &mut t_d);
         let (d_add, d_sub) = diagonal_rlt(m, p, n);
         let mut x_stride = vec![0f32; m * s_x];
         let mut y_stride = vec![0f32; p * s_y];
         pack_stride(&mut x_stride, &x.data, m, p, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
-        stride_rlt_kernel(&x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
+        stride_rlt_kernel(
+            &x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t,
+        );
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
         );
     }
-    fn sfma_rupper_equivalence(m: usize, p: usize, n: usize, s_x:usize, s_y:usize, s_t:usize) {
+    fn sfma_rupper_equivalence(m: usize, p: usize, n: usize, s_x: usize, s_y: usize, s_t: usize) {
         let x = generate_random_matrix(m, p);
         let y = generate_random_matrix(p, n);
         let mut t_d = generate_random_vector(m * n);
@@ -144,20 +137,20 @@ mod test_stride_behavior {
         filter_upper_trapezoid(&mut y_base);
         let mut expected = basic_mult(&x, &y_base);
         increment(&mut expected.data, &t_d, m, n, n, n);
-        // tensor_rut_kernel(&x, &y, &mut t_d);
-        // tensor_rut_block(&x.data, &y.data, &mut t_d, m, p, n, p, n, n);
         let mut x_stride = vec![0f32; m * s_x];
         let mut y_stride = vec![0f32; p * s_y];
         let (d_add, d_sub) = diagonal_rut(m, p, n);
         pack_stride(&mut x_stride, &x.data, m, p, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
-        stride_rut_kernel(&x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
+        stride_rut_kernel(
+            &x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t,
+        );
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
         );
     }
-    fn sfma_ltl_equivalence(m: usize, p: usize, n: usize, s_x:usize, s_y:usize, s_t:usize) {
+    fn sfma_ltl_equivalence(m: usize, p: usize, n: usize, s_x: usize, s_y: usize, s_t: usize) {
         let y = generate_random_matrix(p, n);
         let mut x = generate_random_matrix(m, p);
         let mut x_base = x.clone();
@@ -166,21 +159,20 @@ mod test_stride_behavior {
         x.transpose_inplace(); // stored in transpose
         let mut expected = basic_mult(&x_base, &y);
         increment(&mut expected.data, &t_d, m, n, n, n);
-        // tensor_tlt_block(&x.data, &y.data, &mut t_d, m, p, n, p, n, n);
-        // m, n, n b/c X is s tored in it's transposed state
-        // tensor_tlt_kernel(&x, &y, &mut t_d);
         let mut x_stride = vec![0f32; p * s_x];
         let mut y_stride = vec![0f32; p * s_y];
         let (d_add, d_sub) = diagonal_tlt(m, p, n);
         pack_stride(&mut x_stride, &x.data, p, m, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
-        stride_tlt_kernel(&x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
+        stride_tlt_kernel(
+            &x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t,
+        );
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
         );
     }
-    fn sfma_ltu_equivalence(m: usize, p: usize, n: usize, s_x:usize, s_y:usize, s_t:usize) {
+    fn sfma_ltu_equivalence(m: usize, p: usize, n: usize, s_x: usize, s_y: usize, s_t: usize) {
         let y = generate_random_matrix(p, n);
         let mut x = generate_random_matrix(m, p);
         let mut x_base = x.clone();
@@ -188,19 +180,16 @@ mod test_stride_behavior {
         // filter_lower_trapezoid(&mut x_base);
         filter_upper_trapezoid(&mut x_base);
         x.transpose_inplace(); // stored in transpose
-        // println!("x_base {x_base:?}");
-        // println!("y {y:?}");
         let mut expected = basic_mult(&x_base, &y);
         increment(&mut expected.data, &t_d, m, n, n, n);
-        // m, n, n b/c X is s tored in it's transposed state
-        // tensor_tut_block(&x.data, &y.data, &mut t_d, m, p, n, m, n, n);
-        // tensor_tut_kernel(&x, &y, &mut t_d);
         let mut x_stride = vec![0f32; p * s_x];
         let mut y_stride = vec![0f32; p * s_y];
         let (d_add, d_sub) = diagonal_tut(m, p, n);
         pack_stride(&mut x_stride, &x.data, p, m, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
-        stride_tut_kernel(&x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
+        stride_tut_kernel(
+            &x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t,
+        );
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
