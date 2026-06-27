f1d239cde8c1d83b3bdc0fd05b669fb2ebce75fc
autumn <cyancirrus@gmail.com>
2026-06-26 20:22:32 -0500

    stride tests

--- src/algebra/bmethods/tests/stride.rs
+++ src/algebra/bmethods/tests/stride.rs
@@ -10,20 +10,25 @@ mod test_stride_behavior {
 
     #[test]
     fn test_sfma_equivalence() {
-        let ds_x = 4;
-        let ds_y = 8;
+        let dsx = 4;
+        let dsy = 8;
         for (i, k, j) in test_data() {
-            let s_x = ds_x + k;
-            let s_y = ds_y + j;
-            let s_t = j;
-            sfma_matmul_equivalence(i, k, j, s_x, s_y, s_t);
-            // sfma_tmatmul_equivalence(i, k, j, s_x, s_y, s_t);
-            // sfma_lower_equivalence(i, k, j, s_x, s_y, s_t);
-            // sfma_upper_equivalence(i, k, j, s_x, s_y, s_t);
-            // sfma_rlower_equivalence(i, k, j, s_x, s_y, s_t);
-            // sfma_rupper_equivalence(i, k, j, s_x, s_y, s_t);
-            // sfma_ltl_equivalence(i, k, j, s_x, s_y, s_t);
-            // sfma_ltu_equivalence(i, k, j, s_x, s_y, s_t);
+            let sx = dsx + k;
+            let sy = dsy + j;
+            let st = j;
+            sfma_matmul_equivalence(i, k, j, sx, sy, st);
+            sfma_lower_equivalence(i, k, j, sx, sy, st);
+            sfma_upper_equivalence(i, k, j, sx, sy, st);
+            sfma_rlower_equivalence(i, k, j, sx, sy, st);
+            sfma_rupper_equivalence(i, k, j, sx, sy, st);
+        }
+        for (i, k, j) in test_data() {
+            let sx = dsx + i;
+            let sy = dsy + j;
+            let st = j;
+            sfma_tmatmul_equivalence(i, k, j, sx, sy, st);
+            sfma_ltl_equivalence(i, k, j, sx, sy, st);
+            sfma_ltu_equivalence(i, k, j, sx, sy, st);
         }
     }
     fn sfma_matmul_equivalence(m: usize, p: usize, n: usize, s_x: usize, s_y: usize, s_t: usize) {
@@ -52,13 +57,13 @@ mod test_stride_behavior {
         let x_base = x.clone();
         let mut t_d = generate_random_vector(m * n);
         x.transpose_inplace();
-        let mut x_stride = vec![0f32; m * s_x];
-        let mut y_stride = vec![0f32; p * s_y];
         let mut expected = basic_mult(&x_base, &y);
         increment(&mut expected.data, &t_d, m, n, n, n);
+        let mut x_stride = vec![0f32; p * s_x];
+        let mut y_stride = vec![0f32; p * s_y];
         pack_stride(&mut x_stride, &x.data, p, m, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
-        stride_tkernel(&x.data, &y.data, &mut t_d, m, p, n, s_x, s_y, s_t);
+        stride_tkernel(&x_stride, &y_stride, &mut t_d, m, p, n, s_x, s_y, s_t);
         // tensor_tkernel(&x, &y, &mut t_d);
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
@@ -70,16 +75,17 @@ mod test_stride_behavior {
         let y = generate_random_matrix(p, n);
         let mut x_stride = vec![0f32; m * s_x];
         let mut y_stride = vec![0f32; p * s_y];
-        pack_stride(&mut x_stride, &x.data, m, p, s_x);
-        pack_stride(&mut y_stride, &y.data, p, n, s_y);
         let mut t_d = generate_random_vector(m * n);
         let mut x_base = x.clone();
         filter_lower_trapezoid(&mut x_base);
         let mut expected = basic_mult(&x_base, &y);
         increment(&mut expected.data, &t_d, m, n, n, n);
+        
         let (d_add, d_sub) = diagonal_lt(m, p, n);
+        pack_stride(&mut x_stride, &x.data, m, p, s_x);
+        pack_stride(&mut y_stride, &y.data, p, n, s_y);
         stride_lt_kernel(
-            &x.data, &y.data, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t,
+            &x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t,
         );
         // stride_lt_kernel(&x.data, &y.data, &mut t_d, m, p, n, s_x, s_y, s_t);
         // tensor_lt_kernel(&x, &y, &mut t_d);
@@ -98,12 +104,12 @@ mod test_stride_behavior {
         increment(&mut expected.data, &t_d, m, n, n, n);
         // tensor_ut_block(&x.data, &y.data, &mut t_d, m, p, n, p, n, n);
         // tensor_ut_kernel(&x, &y, &mut t_d);
-         let (d_add, d_sub) = diagonal_ut(m, p, n);
         let mut x_stride = vec![0f32; m * s_x];
         let mut y_stride = vec![0f32; p * s_y];
+        let (d_add, d_sub) = diagonal_ut(m, p, n);
         pack_stride(&mut x_stride, &x.data, m, p, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
-        stride_ut_kernel(&x.data, &y.data, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
+        stride_ut_kernel(&x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
@@ -124,7 +130,7 @@ mod test_stride_behavior {
         let mut y_stride = vec![0f32; p * s_y];
         pack_stride(&mut x_stride, &x.data, m, p, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
-        stride_rlt_kernel(&x.data, &y.data, &mut t_d, d_add, d_sub, m, p, n, p, n, n);
+        stride_rlt_kernel(&x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
@@ -145,7 +151,7 @@ mod test_stride_behavior {
         let (d_add, d_sub) = diagonal_rut(m, p, n);
         pack_stride(&mut x_stride, &x.data, m, p, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
-        stride_rut_kernel(&x.data, &y.data, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
+        stride_rut_kernel(&x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
@@ -163,12 +169,12 @@ mod test_stride_behavior {
         // tensor_tlt_block(&x.data, &y.data, &mut t_d, m, p, n, p, n, n);
         // m, n, n b/c X is s tored in it's transposed state
         // tensor_tlt_kernel(&x, &y, &mut t_d);
-        let mut x_stride = vec![0f32; m * s_x];
+        let mut x_stride = vec![0f32; p * s_x];
         let mut y_stride = vec![0f32; p * s_y];
         let (d_add, d_sub) = diagonal_tlt(m, p, n);
-        pack_stride(&mut x_stride, &x.data, m, p, s_x);
+        pack_stride(&mut x_stride, &x.data, p, m, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
-        stride_tlt_kernel(&x.data, &y.data, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
+        stride_tlt_kernel(&x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
@@ -189,12 +195,12 @@ mod test_stride_behavior {
         // m, n, n b/c X is s tored in it's transposed state
         // tensor_tut_block(&x.data, &y.data, &mut t_d, m, p, n, m, n, n);
         // tensor_tut_kernel(&x, &y, &mut t_d);
-        let mut x_stride = vec![0f32; m * s_x];
+        let mut x_stride = vec![0f32; p * s_x];
         let mut y_stride = vec![0f32; p * s_y];
         let (d_add, d_sub) = diagonal_tut(m, p, n);
-        pack_stride(&mut x_stride, &x.data, m, p, s_x);
+        pack_stride(&mut x_stride, &x.data, p, m, s_x);
         pack_stride(&mut y_stride, &y.data, p, n, s_y);
-        stride_tut_kernel(&x.data, &y.data, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
+        stride_tut_kernel(&x_stride, &y_stride, &mut t_d, d_add, d_sub, m, p, n, s_x, s_y, s_t);
         assert!(
             approx_vector_eq(&expected.data, &t_d[..m * n]),
             "FAILURE WAS ({m:}, {p:}, {n:})"
