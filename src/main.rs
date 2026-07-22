74feb58e41e0597580e2b894f056f8c28ccf8991
autumn <cyancirrus@gmail.com>
2026-07-21 20:16:05 -0500

    before refactor

--- src/main.rs
+++ src/main.rs
@@ -242,7 +242,49 @@ fn complex_eig_pair(h: &mut [f32],  tl: usize, bl:usize) -> bool {
     let d = (h[tl] - h[bl + 1]) / 2f32;
     d * d + h[tl + 1] * h[bl] < EPSILON
 }
-
+fn full_decomp_cpx(h: &mut [f32], r: &mut [f32], w: &mut [f32], mut range: usize, size: usize, mut stride: usize) {
+    let s = range * stride;
+    let mut e1 = s.saturating_sub(stride + 1);
+    let mut e2 = s.saturating_sub(stride + stride + 2);
+    let mut tl = s.saturating_sub(stride + 2);
+    let mut bl = s.saturating_sub(2);
+    let mut curriter = 0;
+    let he1 = h[e1];
+    let he2 = h[e2];
+    let p = &mut [0f32; 3];
+    let mut stall = 0;
+    while range > 0 && curriter < MAX_ITERS {
+        curriter += 1;
+        if h[e1].abs() < TOLERANCE {
+            stall = 0;
+            deflate(1, stride, &mut range, &mut e1, &mut e2, &mut tl, &mut bl, &mut curriter);
+        } else if h[e2].abs() < TOLERANCE {
+            // if e2 == 0 then we are hitting eigen which should be greater than tolerance
+            deflate(2, stride, &mut range, &mut e1, &mut e2, &mut tl, &mut bl, &mut curriter);
+            stall = 0;
+        } else if range == 2 && complex_eig_pair(h, tl, bl) {
+            deflate(2, stride, &mut range, &mut e1, &mut e2, &mut tl, &mut bl, &mut curriter);
+            stall = 0;
+        } else {
+            if range == 2 {
+                francis_iteration_cpx_2x2(h, size, stride, tl, bl);
+            // } else if stall > 0 && (stall + 4) % 10 == 0 {
+            } else if (stall + 8) % 12 == 0 {
+            // } else if (stall + 4) % 10 == 0 {
+            // } else if stall == 6 {
+                exception_shift(h, w, stride, range, tl, bl);
+                francis_iteration_cpx(h, p, w, size, range, stride, tl, bl);
+            } else {
+                double_shift(h, w, stride, range, tl, bl);
+                francis_iteration_cpx(h, p, w, size, range, stride, tl, bl);
+            }
+            stall += 1;
+        }
+    }
+    if range > 1 {
+        println!("missed");
+    }
+}
 fn decomp_cpx(h: &mut [f32], w: &mut [f32], mut range: usize, size: usize, mut stride: usize) {
     let s = range * stride;
     let mut e1 = s.saturating_sub(stride + 1);
@@ -348,6 +390,54 @@ fn exception_shift(
     w[1] = h01 * (h00 + h11 - trace);
     w[2] = h01 * h12;
 }
+
+/// francis_iteration_cpx
+///
+/// * h: hessenberg linearized matrix
+/// * r: rotaiton linearized matrix
+/// * p: projection slice
+/// * w: workspace slice
+/// * size: static number of rows for rotations
+/// * range: number of rows in active window
+/// * stride: stride of the data format
+fn full_francis_iteration_cpx(
+    h: &mut [f32],
+    r: &mut [f32],
+    p: &mut [f32],
+    w: &mut [f32],
+    size: usize,
+    range: usize,
+    stride: usize,
+    tl: usize,
+    bl: usize,
+) {
+    let bound = range.min(3);
+    let p = &mut p[..bound];
+    let tau = params(&mut w[..bound], p);
+    if tau != 0f32 {
+        rapply_householder(h, p, w, tau, size, bound, stride);
+        lapply_householder(h, p, w, tau, bound, range, stride);
+        // ----------------- tracking the rotation matrix
+        rapply_householder(r, p, w, tau, size, bound, stride);
+    }
+    let mut offset = 0;
+    for o in 1..range.saturating_sub(1) {
+        let bound = bound.min(stride - o);
+        let (slice, t) = h.split_at_mut(offset + stride);
+        let slice = &mut slice[offset + o..offset + o + bound];
+        let proj = &mut p[..bound];
+        let tau = params(slice, proj);
+        offset += stride;
+        if tau == 0f32 {
+            continue;
+        }
+        rapply_householder(&mut t[o..], proj, w, tau, size - o, bound, stride);
+        lapply_householder(&mut h[offset..], proj, w, tau, bound, range, stride);
+        // ----------------- tracking the rotation matrix
+        rapply_householder(&mut r[o..], proj, w, tau, size - o, bound, stride);
+    }
+}
+
 /// francis_iteration_cpx
 ///
 /// * h: hessenberg linearized matrix
@@ -468,18 +558,21 @@ fn full_francis_iteration_sym(h: &mut [f32], r: &mut [f32], size: usize, range: 
     apply_g_right(h, 0, 1, stride, range, cosine, -sine);
     apply_gt_left(h, 0, 1, stride, range, cosine, -sine);
     for o in 0..range.saturating_sub(2) {
-        let r = o * stride;
+        let row = o * stride;
         let s1 = o + 1;
         let s2 = o + 2;
         let temp = NdArray {
             dims: vec![range, range],
             data: h.to_vec(),
         };
-        let (_, cosine, sine) = implicit_givens_rotation(h[r + s1], h[r + s2]);
+        let (_, cosine, sine) = implicit_givens_rotation(h[row + s1], h[row + s2]);
+        apply_g_right(&mut h[row..], s1, s2, stride, range-o , cosine, -sine);
+        apply_gt_left(h, s1, s2, stride, range, cosine, -sine);
         // apply_g_right(&mut h[r..], s1, s2, stride, size - o, cosine, -sine);
         // apply_gt_left(h, s1, s2, stride, range.min(s2 + 2), cosine, -sine);
-        apply_g_right(&mut h[r..], s1, s2, stride, range-o , cosine, -sine);
-        apply_gt_left(h, s1, s2, stride, range, cosine, -sine);
+        
+        // -----------------------
+        apply_g_right(&mut r[row..], s1, s2, stride, range-o, cosine, -sine)
     }
 }
 fn generate_symmetric_vector(n: usize) -> Vec<f32> {
