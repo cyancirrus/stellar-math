1ba3c892fc5c296b3845560c8fa38a7225c861e4
autumn <cyancirrus@gmail.com>
2026-08-07 16:26:18 -0500

    found error in the algo

--- src/decomposition/svd/bidiagonalization.rs
+++ src/decomposition/svd/bidiagonalization.rs
@@ -109,15 +109,14 @@ fn full_zero_row(
             cact,
             stride,
         );
-        // w.fill(0f32);
+        w.fill(0f32);
         rapply_householder(
             // &mut v[stride..],
-            &mut v[1..],
+            &mut v[..],
             proj,
             w,
             tau,
             cols,
-            // ract.saturating_sub(1),
             cact,
             stride,
         );
@@ -221,7 +220,7 @@ pub fn full_ubidiagonal(
         // full_zero_col(&mut b[o + k..], &mut u[k..], p, w, rows, ract, cact, stride);
         // zero_row(&mut b[o + 1..], p, w, ract - 1, cact - 1, stride);
         // full_zero_row( &mut b[o + k + 1..], &mut v[o + k + 1..], p, w, cols, ract - 1, cact - 1, stride,);
-        full_zero_row( &mut b[1..], &mut v[o + k ..], p, w, cols, ract - 1, cact - 1, stride,);
+        full_zero_row( &mut b[o + k + 1..], &mut v[k + 1 ..], p, w, cols, ract - 1, cact - 1, stride,);
         ract -= 1;
         cact -= 1;
         o += stride;
