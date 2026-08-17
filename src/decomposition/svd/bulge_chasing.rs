5bc9422491021405e49de8e05c36cf87edd58dea
autumn <cyancirrus@gmail.com>
2026-08-17 17:06:26 -0500

    saving

--- src/decomposition/svd/bulge_chasing.rs
+++ src/decomposition/svd/bulge_chasing.rs
@@ -46,16 +46,25 @@ pub fn decomp_usym(
     }
 }
 
+// A'A
 // (a00, a10) * (a00, a01)
 // (a01 a11)    (a10, a11)
+
+
+// AA'
+// (a00, a01) * (a00, a10)
+// (a10, a11)   (a01, a11)
 
 fn ugivens_iteration(h: &mut [f32], interior: usize, stride: usize, tl: usize, bl: usize) {
     let mut offset = 0;
     // push zero into col
     let sing = singular(h[tl], h[tl + 1], h[bl], h[bl + 1]);
-    let sq_0 = h[0] * h[0] + h[1] * h[stride];
-    let sq_1 = h[0] * h[1] + h[stride] * h[stride + 1];
-    let (_, cos, sin) = implicit_givens_rotation(sq_0 - sing, sq_1);
+    // let sq_00 = h[0] * h[0] + h[1] * h[stride];
+    // let sq_00 = h[0] * h[0] + h[stride] * h[stride];
+    // let sq_01 = h[0] * h[1] + h[stride] * h[stride + 1];
+    let sq_00 = h[0] * h[0];
+    let sq_01 = h[0] * h[1];
+    let (_, cos, sin) = implicit_givens_rotation(sq_00 - sing, sq_01);
     apply_gt_right(h, 0, 1, stride, 2, cos, sin);
     for _ in 0..interior {
         // push zero into row
@@ -71,6 +80,90 @@ fn ugivens_iteration(h: &mut [f32], interior: usize, stride: usize, tl: usize, b
     let (_, cos, sin) = implicit_givens_rotation(h[offset], h[offset + stride]);
     apply_g_left(&mut h[offset..], 0, 1, stride, 2, cos, sin);
 }
+
+
+#[rustfmt::skip]
+pub fn decomp_lsym(
+    b: &mut [f32],
+    card: usize,
+    stride: usize,
+    max_iters:usize,
+    tolerance: f32,
+    absolute: f32,
+) {
+
+    let mut range = card;
+    let mut inter = card.saturating_sub(2);
+    let s = card * stride;
+    // error 1 supra-diagonal above the first real eigen
+    let mut tl = s.saturating_sub(stride + 2);
+    let mut bl = s.saturating_sub(2);
+    let mut e1 = s.saturating_sub(2);
+    let mut curriter = 0;
+    while range > 1 && curriter < max_iters {
+        curriter += 1;
+        let scale = b[tl].abs() + b[bl+1].abs();
+        if b[e1].abs() < (scale * tolerance).min(absolute) {
+            deflate(
+                1,
+                stride,
+                &mut range,
+                &mut inter,
+                &mut e1,
+                &mut tl,
+                &mut bl,
+                &mut curriter,
+            );
+        } else {
+            lgivens_iteration(b, inter, stride, tl, bl);
+        }
+    }
+    if curriter == max_iters {
+        println!("budget-exceeded");
+    }
+}
+
+// (a00, a01) * (a00, a10)
+// (a10, a11)   (a01, a11)
+
+#[rustfmt::skip]
+fn lgivens_iteration(h: &mut [f32], interior: usize, stride: usize, tl: usize, bl: usize) {
+    let mut offset = 0;
+    // push zero into row
+    // let (_, cos, sin) = implicit_givens_rotation(h[0], h[stride]);
+    let sing = singular(h[tl], h[tl + 1], h[bl], h[bl + 1]);
+    // let sq_00 = h[0] * h[0] + h[stride] * h[stride];
+    // let sq_10 = h[0] * h[stride] + h[stride] * h[stride + 1];
+    let sq_00 = h[0] * h[0];
+    let sq_10 = h[0] * h[stride];
+    // let (_, cos, sin) = implicit_givens_rotation(h[0], h[stride]);
+    let (_, cos, sin) = implicit_givens_rotation(sq_00 - sing, sq_10);
+    apply_g_left(h, 0, 1, stride, 2, cos, sin);
+    for _ in 0..interior {
+        // push zero into col
+        let (_, cos, sin) = implicit_givens_rotation(h[offset], h[offset + 1]);
+        apply_gt_right(&mut h[offset ..], 0, 1, stride, 3, cos, sin);
+        // push zero into row
+        offset += stride;
+        let (_, cos, sin) = implicit_givens_rotation(h[offset], h[offset + stride]);
+        apply_g_left(&mut h[offset..], 0, 1, stride, 3, cos, sin);
+        offset += 1;
+    }
+    // // push zero into col
+    let (_, cos, sin) = implicit_givens_rotation(h[offset], h[offset + 1]);
+    apply_gt_right(&mut h[offset ..], 0, 1, stride, 2, cos, sin);
+}
+
+
+
+
+
+
+
+
+
+
+
 #[rustfmt::skip]
 pub fn decomp_lgivens(
     h: &mut [f32],
@@ -105,6 +198,22 @@ pub fn decomp_lgivens(
         subdiag_norm += h[offset + stride].abs();
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 fn iteration_ugivens(h: &mut [f32], interior: usize, stride: usize) -> f32 {
     let mut supdiag_norm = 0f32;
     let mut offset = 0;
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ use stellar::random::generation::generate_random_vector;
 use stellar::structure::ndarray::NdArray;
 
 use stellar::decomposition::svd::bidiagonalization::{lbidiagonal, ubidiagonal};
-use stellar::decomposition::svd::bulge_chasing::decomp_usym;
+use stellar::decomposition::svd::bulge_chasing::{decomp_usym, decomp_lsym};
 #[rustfmt::skip]
 use stellar::decomposition::svd::verify::{
     full_ubidiagonal,
@@ -121,5 +121,30 @@ fn full_upper() {
 }
 
 fn main() {
-    thing_upper();
+    let mut max_iters = 40;
+    let mut tolerance = 1e-6;
+    let mut absolute = 1e-5;
+
+    let rows = 6;
+    let cols = 6;
+    let card = rows.min(cols);
+    let stride = cols;
+    let maximum = rows.max(cols);
+    let mut b = generate_random_vector(rows * cols);
+    let mut p = vec![0f32; maximum];
+    let mut w = vec![0f32; maximum];
+
+    lbidiagonal(&mut b, &mut p, &mut w, rows, cols, card, stride);
+    let bmat = NdArray {
+        dims: vec![rows, cols],
+        data: b.clone(),
+    };
+    println!("bidiagonal {bmat:?}");
+
+    decomp_lsym(&mut b, card, stride, max_iters, tolerance, absolute);
+    let smat = NdArray {
+        dims: vec![rows, cols],
+        data: b.clone(),
+    };
+    println!("singular {smat:?}");
 }
