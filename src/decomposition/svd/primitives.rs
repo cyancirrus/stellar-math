2bd8f8faa22a2883202705f8f2ebf4c7282c89f1
autumn <cyancirrus@gmail.com>
2026-08-18 14:05:45 -0500

    saving

--- src/decomposition/svd/primitives.rs
+++ src/decomposition/svd/primitives.rs
@@ -5,8 +5,8 @@ pub fn singular(m00: f32, m01: f32, m10: f32, m11: f32) -> f32 {
     let s00 = m00 * m00 + m10 * m10;
     let s11 = m01 * m01 + m11 * m11;
     let d = (s00 - s11) / 2f32;
-    let discriminate = d * d + off_diag * off_diag;
-    s11 + d - d.signum() * discriminate.max(0f32).sqrt()
+    let discriminant = d * d + off_diag * off_diag;
+    s11 + d - d.signum() * discriminant.max(0f32).sqrt()
 }
 // element m10 is zero
 pub fn upper_singular(m00: f32, m01: f32, m11: f32) -> f32 {
@@ -14,8 +14,8 @@ pub fn upper_singular(m00: f32, m01: f32, m11: f32) -> f32 {
     let s00 = m00 * m00;
     let s11 = m01 * m01 + m11 * m11;
     let d = (s00 - s11) / 2f32;
-    let discriminate = d * d + s01 * s01;
-    s11 + d - d.signum() * discriminate.max(0f32).sqrt()
+    let discriminant = d * d + s01 * s01;
+    s11 + d - d.signum() * discriminant.max(0f32).sqrt()
 }
 // element m01 is zero
 pub fn lower_singular(m00: f32, m10: f32, m11: f32) -> f32 {
@@ -23,8 +23,8 @@ pub fn lower_singular(m00: f32, m10: f32, m11: f32) -> f32 {
     let s00 = m00 * m00 + m10 * m10;
     let s11 = m11 * m11;
     let d = (s00 - s11) / 2f32;
-    let discriminate = d * d + off_diag * off_diag;
-    s11 + d - d.signum() * discriminate.max(0f32).sqrt()
+    let discriminant = d * d + off_diag * off_diag;
+    s11 + d - d.signum() * discriminant.max(0f32).sqrt()
 }
 pub fn deflate(
     amount: usize,
